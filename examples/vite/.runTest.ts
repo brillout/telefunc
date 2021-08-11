535aa4891ff971c2e4f33ed0bdddc51d536ee743
Romuald Brillout <git@brillout.com>
2021-08-11 15:33:52 +0200

    inspect windows bug

--- examples/vite/.runTest.ts
+++ examples/vite/.runTest.ts
@@ -7,10 +7,14 @@ function runTest(cmd: "npm run dev" | "npm run prod") {
 
   test("remote shell with telefunc", async () => {
     expect(1).toBe(1);
-    /*
+    console.log(1)
     page.goto(`${urlBase}/`);
+    console.log(2)
     expect(await page.textContent("body")).not.toContain("node_modules");
+    console.log(3)
     await page.click("button");
+    console.log(4)
+    /*
     await autoRetry(async () => {
       expect(await page.textContent("body")).toContain("node_modules");
     });
