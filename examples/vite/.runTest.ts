5ce74690b3971b09ada3ebbce2c299b629682cfc
Romuald Brillout <git@brillout.com>
2021-08-11 16:25:55 +0200

    inspect windows bug

--- examples/vite/.runTest.ts
+++ examples/vite/.runTest.ts
@@ -1,4 +1,5 @@
 import { page, run, urlBase, autoRetry } from "../../libframe/test/setup";
+import { sleep } from "../../libframe/test/utils";
 
 export { runTest };
 
@@ -9,11 +10,16 @@ function runTest(cmd: "npm run dev" | "npm run prod") {
     page.goto(`${urlBase}/`);
     expect(await page.textContent("body")).not.toContain("node_modules");
     await page.click("button#cmd-dir");
-    let n = 0
-    let start = new Date().getTime()
+    let n = 0;
+    let start = new Date().getTime();
+    console.log(31)
+    await sleep(5000);
+    console.log(32)
+    /*
     await autoRetry(async () => {
-      // console.log(++n, ((new Date().getTime() - start)/1000))
+      console.log(++n, (new Date().getTime() - start) / 1000);
       expect(await page.textContent("body")).toContain("node_modules");
     });
+    */
   });
 }
