d71ae1fc8fcedd471affb394d6308963d677b352
Romuald Brillout <git@brillout.com>
2021-09-06 17:34:55 +0200

    docs: move nav header code to user land

---
+++ docs/renderer/NavHeader.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import logoUrl from '../icons/telefunc.svg'
+
+export { NavHeader }
+export { NavHeaderMobile }
+
+function NavHeaderMobile() {
+  const LOGO_SIZE = 40
+  return (
+    <>
+      <img src={logoUrl} height={LOGO_SIZE} width={LOGO_SIZE} />
+      <HeaderTitle fontSize={'1.55em'} marginLeft={6} />
+    </>
+  )
+}
+
+function NavHeader() {
+  const LOGO_SIZE = 55
+  return (
+    <>
+      <img src={logoUrl} height={LOGO_SIZE} width={LOGO_SIZE} />
+      <HeaderTitle fontSize={'2.55em'} marginLeft={10} />
+    </>
+  )
+}
+
+function HeaderTitle({ fontSize, marginLeft }: { fontSize: string; marginLeft: number }) {
+  return (
+    <span
+      style={{
+        fontSize,
+        marginLeft
+      }}
+    >
+      {'Telefunc'}
+    </span>
+  )
+}
--- docs/renderer/_default.page.server.tsx
+++ docs/renderer/_default.page.server.tsx
@@ -1,13 +1,17 @@
 export * from "libframe-docs/_default.page.server";
-import logoUrl from "../icons/telefunc.svg";
 import { setFrame } from "libframe-docs/_default.page.server";
 import { headings } from "../headings";
 import { projectInfo } from "../utils";
+import logoUrl from '../icons/telefunc.svg'
+import React from 'react'
+import { NavHeader, NavHeaderMobile } from "./NavHeader";
 
 setFrame({
   projectVersion: projectInfo.version,
   projectName: projectInfo.name,
   repo: projectInfo.githubRepository,
-  headings,
   logoUrl,
+  headings,
+  navHeader: <NavHeader />,
+  navHeaderMobile: <NavHeaderMobile />,
 });
