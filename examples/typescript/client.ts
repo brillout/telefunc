cf9941d4e70230e8a199b575d095d199cc483522
Romuald Brillout <git@brillout.com>
2021-09-09 15:31:46 +0200

    add typescript example

---
+++ examples/typescript/client.ts
@@ -0,0 +1,9 @@
+import { getPerson } from './persons.telefunc'
+
+main()
+
+async function main() {
+  const person = await getPerson(0)
+  const html = `First name: ${person.firstName}<br/>Last name: ${person.lastName}`
+  document.getElementById('view')!.innerHTML = html
+}
---
+++ examples/typescript/readme.md
@@ -0,0 +1,10 @@
+Example of using Telefunc with TypeScript.
+
+To run it:
+
+```bash
+git clone git@github.com:brillout/telefunc
+cd telefunc/examples/typescript/
+npm install
+npm run start
+```
