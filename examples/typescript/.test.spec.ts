96e01dc865bf4daf56723993d2b6d2772e86efa2
Romuald Brillout <git@brillout.com>
2021-09-09 16:19:10 +0200

    fix test

--- examples/typescript/.test.spec.ts
+++ examples/typescript/.test.spec.ts
@@ -2,8 +2,8 @@ import { page, run, urlBase } from '../../libframe/test/setup'
 
 run('npm run start')
 
-test('remote shell with telefunc', async () => {
-  page.goto(`${urlBase}/`)
+test('telefunction call', async () => {
+  await page.goto(`${urlBase}/`)
   const text = await page.textContent('#view')
   expect(text).toContain('First name: Alan')
   expect(text).toContain('Last name: Turing')
