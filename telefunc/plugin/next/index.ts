e24e0d363cbbcab3d91b2ed46ecbdf87f5f1e388
aslemammad <zorofight94@gmail.com>
2021-11-05 12:29:23 +0330

    feat: basic nextjs plugin

--- telefunc/plugin/next/index.ts
+++ telefunc/plugin/next/index.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from 'next'
+import { WebpackCompiler } from 'unplugin'
 
 export default plugin
 
-function plugin(): NextConfig {
-  return {
-    webpack: (config: NextConfig, { dev, isServer, dir, config: resolvedConfig, defaultLoaders }) => {
-      console.log(config, dev, isServer, dir)
+function plugin(nextConfig: NextConfig = {}): NextConfig {
+  return Object.assign({}, nextConfig, {
+    webpack: (config, options) => {
+      // console.log(options.config.compress)
 
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
       return config
     },
-  }
+  } as NextConfig)
 }
