7581fc131aaf4136541cfaeee1bae730f4c776c7
aslemammad <zorofight94@gmail.com>
2021-11-08 22:54:21 +0330

    experimental server loader

--- telefunc/plugin/next/loader.ts
+++ telefunc/plugin/next/loader.ts
@@ -1,52 +1,54 @@
-import { dirname, join, resolve } from 'path'
+import { dirname, join, relative, resolve } from 'path'
+import { readFile } from 'fs/promises'
 import type { Compiler, LoaderDefinitionFunction } from 'webpack'
 import { assert, moduleExists } from '../../server/utils'
 import { isSSR } from './isSSR'
 
+const isWin = process.platform === 'win32'
+
+export type PromiseType<T extends Promise<any>> = T extends Promise<infer U> ? U : never
+
 module.exports = async function (input) {
   const compiler = this._compiler!
 
-  let entry: Record<string, unknown>
-  if (typeof compiler.options.entry === 'function') {
-    entry = await compiler.options.entry()
-  } else {
-    entry = compiler.options.entry
-  }
-  console.log(compiler.name)
+
   if (isSSR(compiler.name)) {
-    return serverLoader.call(this, input, compiler)
+    return serverLoader.call(this, input)
   } else {
-    return clientLoader.call(this, input, compiler)
+    return clientLoader.call(this, input)
   }
 } as LoaderDefinitionFunction
 
-const serverLoader: LoaderDefinitionFunction = async function serverLoader(input: string): string {
-
+const serverLoader: LoaderDefinitionFunction = async function serverLoader(input: string) {
   let entry = await resolveWebpackEntry(this._compiler!)
 
-  console.log(entry)
-  // const telefuncDist = resolve(dirname(require.resolve('telefunc')), '../../../dist/')
-  // {
-  //   const filePath = join(telefuncDist, '/esm/plugin/webpack/importTelefuncFiles.js')
-  //   assert(moduleExists(filePath))
-  //   entry['server/importTelefuncFiles'] = {
-  //     import: [filePath],
-  //   }
-  // }
-  // {
-  //   const filePath = join(telefuncDist, '/esm/plugin/webpack/importBuild.js')
-  //   assert(moduleExists(filePath))
-  //   entry['server/importBuild'] = {
-  //     import: [filePath],
-  //   }
-  // }
+  let root = process.cwd()
+  // console.log(this._compiler.options.externals )
+  const telefuncDist = resolve(dirname(require.resolve('telefunc')), '../../../dist/')
+
+  {
+    const rootPathForWin = relative(__dirname, root).replace(/\\/g, '/')
+    const filePath = join(telefuncDist, '/esm/plugin/webpack/importTelefuncFiles.js')
+    const fileContent = await readFile(filePath, 'utf-8')
+    this.emitFile(
+      'importTelefuncFiles.js',
+      fileContent.replace('@telefunc/REPLACE_PATH', isWin ? rootPathForWin : root),
+    )
+    assert(moduleExists(filePath))
+  }
+
+  {
+    const filePath = join(telefuncDist, '/esm/plugin/webpack/importBuild.js')
+    assert(moduleExists(filePath))
+  }
+
+  updateWebpackEntry(this._compiler!, entry)
+  // console.log(entry)
   return input
 }
 
-function clientLoader(input: string, compiler: Compiler): string {
-
+const clientLoader: LoaderDefinitionFunction = function clientLoader(input: string): string {
   return input
-
 }
 
 const resolveWebpackEntry = async (compiler: Compiler) => {
@@ -55,5 +57,16 @@ const resolveWebpackEntry = async (compiler: Compiler) => {
     return entry()
   } else {
     return entry
+  }
+}
+const updateWebpackEntry = async (
+  compiler: Compiler,
+  newEntry: PromiseType<ReturnType<typeof resolveWebpackEntry>>,
+) => {
+  newEntry = {}
+  if (typeof compiler.options.entry === 'function') {
+    compiler.options.entry = async () => newEntry
+  } else {
+    compiler.options.entry = newEntry
   }
 }
