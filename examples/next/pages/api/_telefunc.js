c2062480d09dfd67363de08fc7b7c72fc6288ba2
aslemammad <zorofight94@gmail.com>
2021-11-09 09:15:56 +0330

    import telefunctions

--- examples/next/pages/api/_telefunc.js
+++ examples/next/pages/api/_telefunc.js
@@ -1,8 +1,11 @@
+import * as personsTelefunctions from '../../telefunc/persons.telefunc'
 import { createTelefuncCaller } from 'telefunc'
 
+console.log(personsTelefunctions)
 const callTelefunc = createTelefuncCaller({ isProduction: process.env.NODE_ENV === 'production', root: process.cwd() })
 
 export default async function _telefunc(req, res) {
+  console.log(req.method)
   const { originalUrl: url, method, body, headers } = req
   const userAgent = headers['user-agent']
   const context = {
