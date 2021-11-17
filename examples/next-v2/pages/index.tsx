269845ff3ac437147bb2feacdc446e7b28d219d4
aslemammad <zorofight94@gmail.com>
2021-11-18 01:10:56 +0330

    base next-v2 example

---
+++ examples/next-v2/pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import { getPerson, Person, getIsServer } from '../telefunc/persons.telefunc'
+
+const Home: NextPage = () => {
+  const [person, setPersion] = useState<null | Person>()
+  const [isServer, setIsServer] = useState<boolean>()
+
+  const fetchPerson = () => {
+    getPerson(0).then((person) => {
+      setPersion(person)
+    })
+    getIsServer().then((isServer) => {
+      setIsServer(isServer)
+    })
+  }
+
+  useEffect(() => {
+    fetchPerson()
+  }, [])
+
+  return (
+    <div id="view">
+      First name: {person?.firstName}
+      <br />
+      Last name: {person?.lastName}
+      <br />
+      server: {isServer ? 'true' : 'false'}
+    </div>
+  )
+}
+
+export default Home
