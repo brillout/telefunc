f69104acee3f86c704083dc14381c0add3958779
aslemammad <zorofight94@gmail.com>
2021-11-18 01:34:41 +0330

    telefunc glob

--- telefunc/plugin/next/transformTelefuncRouteFile.ts
+++ telefunc/plugin/next/transformTelefuncRouteFile.ts
@@ -1,9 +1,13 @@
-import glob from 'fast-glob'
+import * as glob from 'fast-glob'
+import { dirname, relative } from 'path'
 
 export { transformTelefuncRouteFile }
 
 async function transformTelefuncRouteFile(src: string, id: string, root: string) {
-  console.log(src, id, root)
+  const currentDir = dirname(id)
+  const relativeRoot = relative(currentDir, root)
+  const files = await glob(`${relativeRoot}/**/*.telefunc.*`, { cwd: currentDir })
+  console.log(files)
   return {
     code: '',
     map: null,
