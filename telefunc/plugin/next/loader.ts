4318d9af81c37514ec4cc80055a8b9b8a13a7c3e
aslemammad <zorofight94@gmail.com>
2021-11-18 01:11:39 +0330

    isTelefuncRouteFile filter

--- telefunc/plugin/next/loader.ts
+++ telefunc/plugin/next/loader.ts
@@ -2,6 +2,7 @@ import type { LoaderDefinitionFunction } from 'webpack'
 import { toPosixPath } from '../../server/utils'
 import { isSSR } from './isSSR'
 import { transformTelefuncFile } from '../transformTelefuncFile'
+import { transformTelefuncRouteFile } from './transformTelefuncRouteFile'
 
 export type PromiseType<T extends Promise<any>> = T extends Promise<infer U> ? U : never
 
@@ -9,6 +10,12 @@ module.exports = async function (input) {
   const compiler = this._compiler!
   const id = this.resource
   const root = this._compiler!.context
+
+  if (isTelefuncRouteFile(id)) {
+    const { code } = await transformTelefuncRouteFile(input, id, root)
+    return code
+  }
+
   if (isSSR(compiler.name)) {
     return input
   }
@@ -16,3 +23,7 @@ module.exports = async function (input) {
   const { code } = await transformTelefuncFile(input, toPosixPath(id), toPosixPath(root))
   return code
 } as LoaderDefinitionFunction
+
+function isTelefuncRouteFile(id: string) {
+  return id.includes('_telefunc')
+}
