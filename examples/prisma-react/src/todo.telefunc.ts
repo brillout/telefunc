d339b3d2e1e1c848df5f13dace1f2a787f8c523f
aslemammad <zorofight94@gmail.com>
2021-11-18 18:08:06 +0330

    toggleTodo + deleteTodo

--- examples/prisma-react/src/todo.telefunc.ts
+++ examples/prisma-react/src/todo.telefunc.ts
@@ -2,18 +2,54 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export { addTodo, getTodos }
+export { addTodo, getTodos, toggleTodo, deleteTodo }
+
+async function getTodos() {
+  const todos = await prisma.todo.findMany()
+  return todos
+}
 
 async function addTodo({ title, content }: { title: string; content: string }) {
   await prisma.todo.create({
     data: {
       title,
       content,
+      completed: false,
     },
   })
 }
 
-async function getTodos() {
-  const todos = await prisma.todo.findMany()
-  return todos
+async function toggleTodo(id: number) {
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id,
+    },
+  })
+  if (!todo) {
+    throw new Error('Todo not found')
+  }
+  await prisma.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      completed: !todo.completed,
+    },
+  })
+}
+
+async function deleteTodo(id: number) {
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id,
+    },
+  })
+  if (!todo) {
+    throw new Error('Todo not found')
+  }
+  await prisma.todo.delete({
+    where: {
+      id,
+    },
+  })
 }
