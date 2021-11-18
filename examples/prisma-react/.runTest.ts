4ca24dc7873d6021ea1268b4d5ffceb8ee9630f4
aslemammad <zorofight94@gmail.com>
2021-11-18 19:16:56 +0330

    test cases

---
+++ examples/prisma-react/.runTest.ts
@@ -0,0 +1,33 @@
+import { page, run, urlBase, autoRetry, fetchHtml } from '../../libframe/test/setup'
+
+export { runTest }
+
+function runTest(cmd: 'npm run dev' | 'npm run prod') {
+  run(cmd)
+
+  test('Add to-do item', async () => {
+    await page.goto(`${urlBase}/`)
+    expect(await page.textContent('body')).toContain('todo list')
+    await page.waitForSelector('fieldset:not([disabled])')
+    await page.fill('input[placeholder="Title"]', 'title')
+    await page.fill('input[placeholder="Content"]', 'content')
+    await page.click('button[type="submit"]')
+    await autoRetry(async () => {
+      expect(await page.textContent('body')).toContain('title')
+      expect(await page.textContent('body')).toContain('content')
+    })
+  })
+
+  test('New to-do item is persisted & rendered to HTML', async () => {
+    const html = await fetchHtml('/')
+    expect(html).toContain('title')
+    expect(html).toContain('content')
+  })
+
+  test('toggle item', async () => {
+    await page.click('button[id="toggle-title"]')
+    await autoRetry(async () => {
+      expect(await page.textContent('body')).toContain('done')
+    })
+  })
+}
