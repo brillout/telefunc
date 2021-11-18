1ed5bc31b9feba81eead1b5f9a5c712f60a3eb9c
aslemammad <zorofight94@gmail.com>
2021-11-18 12:05:26 +0330

    addTelefunction to __internal_addTelefunction

--- telefunc/server/index.ts
+++ telefunc/server/index.ts
@@ -2,7 +2,10 @@ import { assertEnv } from './assertEnv'
 export { createTelefuncCaller } from './createTelefuncCaller'
 export { getContext, provideContext } from './getContext'
 export { Abort } from './Abort'
-export { setTelefuncFiles as __internal_setTelefuncFiles } from './callTelefunc'
+export {
+  setTelefuncFiles as __internal_setTelefuncFiles,
+  addTelefunction as __internal_addTelefunction,
+} from './callTelefunc'
 export { shield } from './shield'
 
 assertEnv()
