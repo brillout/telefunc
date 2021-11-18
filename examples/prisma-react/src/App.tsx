a07efe35285f252e4dafe3ba5d9154e066a112f0
aslemammad <zorofight94@gmail.com>
2021-11-18 15:58:50 +0330

    remove additional files

--- examples/prisma-react/src/App.tsx
+++ examples/prisma-react/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
