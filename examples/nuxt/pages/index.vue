f2bf2d79026c27f644e7d92562e086a57f198663
aslemammad <zorofight94@gmail.com>
2021-11-20 17:02:25 +0330

    vue index page

---
+++ examples/nuxt/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="view">
+    First name: {{ firstName }}
+    <br />
+    Last name: {{ lastName }}
+    <br />
+    server: {{ isServer ? 'true' : 'false' }}
+  </div>
+</template>
+
+<script>
+// I'd like to see how vue devs would refactor this, as I am a React dev
+
+export default {
+  mounted: function () {
+    this.$store.dispatch('fetch')
+  },
+  computed: {
+    firstName: function () {
+      return this.$store.state.firstName
+    },
+    lastName: function () {
+      return this.$store.state.lastName
+    },
+    isServer: function () {
+      return this.$store.state.isServer
+    },
+  },
+}
+</script>
