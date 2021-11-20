0dd4600cf897534874c88e6c66076277959ea506
aslemammad <zorofight94@gmail.com>
2021-11-20 17:02:02 +0330

    runTest

---
+++ examples/nuxt/.runTest.ts
@@ -0,0 +1,18 @@
+import { page, urlBase, autoRetry, run } from '../../libframe/test/setup'
+
+export { runTest }
+
+function runTest(cmd: 'npm run dev' | 'npm run prod') {
+  run(cmd)
+
+  test('telefunction call', async () => {
+    await page.goto(`${urlBase}/`)
+    // `autoRetry` to ensure JavaScript has loaded & executed
+    await autoRetry(async () => {
+      const text = await page.textContent('#view')
+      expect(text).toContain('First name: Alan')
+      expect(text).toContain('Last name: Turing')
+      expect(text).toContain('server: true')
+    })
+  })
+}
