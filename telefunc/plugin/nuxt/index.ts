f15785c773bacf5ab263258faab5bae80065ad9d
aslemammad <zorofight94@gmail.com>
2021-11-21 14:17:34 +0330

    build + middleware

--- telefunc/plugin/nuxt/index.ts
+++ telefunc/plugin/nuxt/index.ts
@@ -1,10 +1,18 @@
-export default telefuncModule
+import type { Module } from '@nuxt/types'
+import { resolve } from 'path'
+import { _telefunc } from './_telefunc'
 
+const _telefuncModule: Module = function telefuncModule() {
+  this.extendBuild((config) => {
+    config.module!.rules.push({
+      test: /\.telefunc\./,
+      use: [{ loader: resolve(__dirname, './loader.js') }],
+    })
+  })
 
-function telefuncModule(moduleOptions: any) {
-  console.log(moduleOptions, this)
-
+  this.addServerMiddleware(_telefunc)
 }
 
+export default _telefuncModule
 
 module.exports.meta = require('../../../../package.json')
