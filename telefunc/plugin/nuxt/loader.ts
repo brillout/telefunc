2a907c69532fbd53b17fad4f75c59b1b54523c64
aslemammad <zorofight94@gmail.com>
2021-11-22 00:07:40 +0330

    add ssr transform

--- telefunc/plugin/nuxt/loader.ts
+++ telefunc/plugin/nuxt/loader.ts
@@ -1,29 +1,20 @@
 import type { LoaderDefinitionFunction } from 'webpack'
 import { toPosixPath } from '../../server/utils'
-import { isSSR } from '../next/isSSR'
-import { transformTelefuncFile } from '../transformTelefuncFile'
 import { isTelefuncFile } from '../isTelefuncFile'
+import { isSSR } from '../next/isSSR'
+import { transformTelefuncFileSSR } from '../next/transformTelefuncFileSSR'
+import { transformTelefuncFile } from '../transformTelefuncFile'
 
 export type PromiseType<T extends Promise<any>> = T extends Promise<infer U> ? U : never
 
 module.exports = async function (input) {
-  return input
   const compiler = this._compiler!
   const id = this.resource
   const root = this._compiler!.context
-  console.log(compiler.name)
 
-  // if (isTelefuncRouteFile(id) && input.includes('@telefunc')) {
-  //   const { code } = await transformTelefuncRouteFile(input, id, root)
-  //   return code
-  // }
-
-  // if (isTelefuncFile(id) && isSSR(compiler.name)) {
-  //   const { code } = await transformTelefuncFileSSR(input, id, root)
-  //   return code
-  // }
-  if (isSSR(compiler.name)) {
-    return input
+  if (isTelefuncFile(id) && isSSR(compiler.name)) {
+    const { code } = await transformTelefuncFileSSR(input, id, root)
+    return code
   }
 
   const { code } = await transformTelefuncFile(input, toPosixPath(id), toPosixPath(root))
