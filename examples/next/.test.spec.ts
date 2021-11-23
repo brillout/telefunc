a2cd6fd1fa454b5a9930b85ecbd6f308591819fc
Romuald Brillout <git@brillout.com>
2021-11-23 16:38:46 +0100

    attempt: fix test

--- examples/next/.test.spec.ts
+++ examples/next/.test.spec.ts
@@ -1,16 +1,32 @@
 import { page, run, urlBase, autoRetry } from '../../libframe/test/setup'
 
-jest.setTimeout(50000)
-run('npm run prod')
-
+run('npm run prod', {
+  serverIsRunningMessage: 'started server on',
+  debug: true,
+  additionalTimeout: 240 * 1000,
+  serverIsRunningDelay: 20 * 1000,
+})
 
 test('telefunction call', async () => {
+  await sleep(20 * 1000)
+  console.log('b1')
   await page.goto(`${urlBase}/`)
-  // `autoRetry` to ensure JavaScript has loaded & executed
-  await autoRetry(async () => {
-    const text = await page.textContent('#view')
-    expect(text).toContain('First name: Alan')
-    expect(text).toContain('Last name: Turing')
-    expect(text).toContain('server: true')
-  })
+  console.log('b2')
+
+  // `autoRetry` to ensure async remote Telefunction call has finished
+  await autoRetry(
+    async () => {
+      const text = await page.textContent('#view')
+      expect(text).toContain('First name: Alan')
+    },
+    { timeout: 120 * 1000 },
+  )
+
+  const text = await page.textContent('#view')
+  expect(text).toContain('Last name: Turing')
+  expect(text).toContain('server: true')
 })
+
+function sleep(milliseconds: number): Promise<void> {
+  return new Promise((r) => setTimeout(r, milliseconds))
+}
