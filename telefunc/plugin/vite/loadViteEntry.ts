4bbfb816d4489bfbe9357a69fce3e93e6153600a
Romuald Brillout <git@brillout.com>
2021-11-23 15:45:28 +0100

    fix: avoid Next.js from statically analyzing `require()` calls

--- telefunc/plugin/vite/loadViteEntry.ts
+++ telefunc/plugin/vite/loadViteEntry.ts
@@ -1,4 +1,4 @@
-import { assert, assertUsage, moduleExists } from '../../server/utils'
+import { assert, assertUsage, moduleExists, nodeRequire } from '../../server/utils'
 import { resolve as pathResolve } from 'path'
 import type { ViteDevServer } from 'vite'
 
@@ -21,10 +21,10 @@ async function loadViteEntry({
   if (isProduction) {
     const prodPathResolved = pathResolve(prodPath)
     assertUsage(moduleExists(prodPathResolved), `${errorMessage}. (Build file ${prodPathResolved} is missing.)`)
-    moduleExports = require_(prodPathResolved)
+    moduleExports = nodeRequire(prodPathResolved)
   } else {
     assert(viteDevServer)
-    devPath = requireResolve(devPath)
+    devPath = nodeRequire.resolve(devPath)
     assert(moduleExists(devPath))
     try {
       moduleExports = await viteDevServer.ssrLoadModule(devPath)
@@ -34,15 +34,4 @@ async function loadViteEntry({
     }
   }
   return moduleExports
-}
-
-function require_(modulePath: string): unknown {
-  // `req` instead of `require` so that Webpack doesn't do dynamic dependency analysis
-  const req = require
-  return req(modulePath)
-}
-function requireResolve(modulePath: string): string {
-  // `req` instead of `require` so that Webpack doesn't do dynamic dependency analysis
-  const req = require
-  return req.resolve(modulePath)
 }
