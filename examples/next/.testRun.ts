bd3c219ea89b65e13d1e3230b4290a64db3f11ab
aslemammad <zorofight94@gmail.com>
2021-11-24 13:50:05 +0330

    remove autoRetry

--- examples/next/.testRun.ts
+++ examples/next/.testRun.ts
@@ -1,9 +1,9 @@
-import { page, run, urlBase, autoRetry, isGithubAction } from '../../libframe/test/setup'
+import { page, run, urlBase, autoRetry, isGitHubAction } from '../../libframe/test/setup'
 
 export { testRun }
 
 function testRun(cmd: 'npm run prod' | 'npm run dev') {
-  if (cmd === 'npm run prod' && isGithubAction()) {
+  if (cmd === 'npm run prod' && isGitHubAction()) {
     test("SKIPED: Next.js prod build doesn't work in GitHub Actions", () => {})
     return
   }
@@ -20,13 +20,10 @@ function testRun(cmd: 'npm run prod' | 'npm run dev') {
   test('telefunction call', async () => {
     await page.goto(`${urlBase}/`)
 
-    // `autoRetry` to ensure async remote Telefunction call has finished
-    await autoRetry(async () => {
-      const text = await page.textContent('#view')
-      expect(text).toContain('First name: Alan')
-    })
-
     const text = await page.textContent('#view')
+
+    expect(text).toContain('First name: Alan')
+
     expect(text).toContain('Last name: Turing')
     expect(text).toContain('server: true')
   })
