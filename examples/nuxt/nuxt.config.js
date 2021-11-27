d32f29340aca498ccc114b4f48657b1a9bfa684d
aslemammad <zorofight94@gmail.com>
2021-11-28 01:49:55 +0330

    nuxt config clean

--- examples/nuxt/nuxt.config.js
+++ examples/nuxt/nuxt.config.js
@@ -16,14 +16,6 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
@@ -31,8 +23,4 @@ export default {
   modules: [
     "telefunc/nuxt"
   ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
 }
