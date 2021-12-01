3aca398ac40672aa9a091a455f80dedb8a79b533
Romuald Brillout <git@brillout.com>
2021-12-01 15:43:49 +0100

    fix nuxt test

--- examples/nuxt/.runTest.ts
+++ examples/nuxt/.runTest.ts
@@ -1,11 +1,12 @@
-import { page, urlBase, autoRetry, run, isLinux } from '../../libframe/test/setup'
+import { page, urlBase, autoRetry, run } from '../../libframe/test/setup'
 import { NUXT_APP_IS_READY } from './NUXT_APP_IS_READY'
 
 export { runTest }
 
 function runTest(cmd: 'npm run dev' | 'npm run prod') {
-  const serverIsReadyDelay = !isLinux() ? 10 * 1000 : undefined
-  run(cmd, { serverIsReadyMessage: NUXT_APP_IS_READY, serverIsReadyDelay })
+  const serverIsReadyDelay = 30 * 1000
+  const additionalTimeout = serverIsReadyDelay
+  run(cmd, { serverIsReadyMessage: NUXT_APP_IS_READY, serverIsReadyDelay, additionalTimeout })
 
   test('telefunction call', async () => {
     await page.goto(`${urlBase}/`)
