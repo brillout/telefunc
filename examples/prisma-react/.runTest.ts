1b9a796591dda24a48e2269c62e33fd087dfe533
aslemammad <zorofight94@gmail.com>
2021-12-07 18:47:05 +0330

    remove log

--- examples/prisma-react/.runTest.ts
+++ examples/prisma-react/.runTest.ts
@@ -26,7 +26,6 @@ function runTest(cmd: 'npm run test:dev' | 'npm run test:prod') {
   test('toggle item', async () => {
     await page.click('button[id="toggle-title"]')
     await autoRetry(async () => {
-      console.log(await page.content())
       expect(await page.textContent('body')).toContain('done')
     })
   })
