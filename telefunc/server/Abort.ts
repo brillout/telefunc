222398a9aab34820a530495b0809b53693a0b390
Romuald Brillout <git@brillout.com>
2021-12-27 13:11:35 +0100

    docs: permissions - wip

--- telefunc/server/Abort.ts
+++ telefunc/server/Abort.ts
@@ -8,5 +8,5 @@ function Abort(this: void, ...args: never[]) {
     'Using superflous `new` operator: use `throw Abort()` instead of `throw new Abort()`.',
   )
   assertUsage(args.length === 0, "Abort() doesn't accept any argument. Consider returning a JavaScript value instead, see https://telefunc.com/permission")
-  return new Error('Abort. (This error is not shown in production, see https://telefunc.com/permission)')
+  return new Error('Abort')
 }
