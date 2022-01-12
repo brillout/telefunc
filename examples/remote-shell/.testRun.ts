63d667d2447ed11c0b64261934c49ba5ef72ee19
Romuald Brillout <git@brillout.com>
2022-01-12 22:59:18 +0100

    adjust test timeout

--- examples/remote-shell/.testRun.ts
+++ examples/remote-shell/.testRun.ts
@@ -1,4 +1,4 @@
-import { page, run, urlBase, autoRetry, isWindows } from '../../libframe/test/setup'
+import { page, run, urlBase, autoRetry, isWindows, isLinux, isGithubAction } from '../../libframe/test/setup'
 
 export { testRun }
 
@@ -11,9 +11,12 @@ function testRun(cmd: 'npm run dev' | 'npm run prod') {
     expect(await page.textContent('body')).not.toContain('node_modules')
     await page.click('button#cmd-ls')
     // `autoRetry` to ensure JavaScript has loaded & executed
-    await autoRetry(async () => {
-      expect(await page.textContent('body')).toContain('node_modules')
-    })
+    await autoRetry(
+      async () => {
+        expect(await page.textContent('body')).toContain('node_modules')
+      },
+      { timeout: isLinux() || !isGithubAction() ? undefined : 200 * 1000 },
+    )
   })
 
   test('telefunc context', async () => {
