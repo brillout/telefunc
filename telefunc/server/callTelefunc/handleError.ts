1f8fd88090643e19b9aed81fd0151d02aef17b72
Romuald Brillout <git@brillout.com>
2022-01-15 10:35:27 +0100

    refactor

--- telefunc/server/callTelefunc/handleError.ts
+++ telefunc/server/callTelefunc/handleError.ts
@@ -3,39 +3,31 @@ export { handleError }
 import type { ViteDevServer } from 'vite'
 import { hasProp } from '../utils'
 
-function handleError(err: unknown, callContext: { _isProduction: boolean; _viteDevServer: ViteDevServer | null }) {
+function handleError(err: unknown, callContext: { _viteDevServer: ViteDevServer | null }) {
   // We ensure we print a string; Cloudflare Workers doesn't seem to properly stringify `Error` objects.
   const errStr = (hasProp(err, 'stack') && String(err.stack)) || String(err)
-  if (!callContext._isProduction && callContext._viteDevServer) {
-    // TODO: check if Vite already logged the error
-  }
 
   if (viteAlreadyLoggedError(err, callContext)) {
     return
   }
-  viteErrorCleanup(err, callContext._viteDevServer)
+  viteErrorCleanup(err, callContext)
 
   console.error(errStr)
 }
 
-function viteAlreadyLoggedError(
-  err: unknown,
-  callContext: { _isProduction: boolean; _viteDevServer: ViteDevServer | null },
-) {
-  if (callContext._isProduction) {
+function viteAlreadyLoggedError(err: unknown, callContext: { _viteDevServer: ViteDevServer | null }) {
+  if (!callContext._viteDevServer) {
     return false
   }
-  if (callContext._viteDevServer && callContext._viteDevServer.config.logger.hasErrorLogged(err as Error)) {
-    return true
-  }
-  return false
+  return callContext._viteDevServer.config.logger.hasErrorLogged(err as Error)
 }
 
-function viteErrorCleanup(err: unknown, viteDevServer: ViteDevServer | null) {
-  if (viteDevServer) {
-    if (hasProp(err, 'stack')) {
-      // Apply source maps
-      viteDevServer.ssrFixStacktrace(err as Error)
-    }
+function viteErrorCleanup(err: unknown, callContext: { _viteDevServer: ViteDevServer | null }) {
+  if (!callContext._viteDevServer) {
+    return false
+  }
+  if (hasProp(err, 'stack')) {
+    // Apply source maps
+    callContext._viteDevServer.ssrFixStacktrace(err as Error)
   }
 }
