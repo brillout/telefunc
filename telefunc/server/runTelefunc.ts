b5c32d5ad982e28d1255b2bd7c122d6d015e6d2c
Romuald Brillout <git@brillout.com>
2022-01-22 23:23:53 +0100

    improve wrong usage message

--- telefunc/server/runTelefunc.ts
+++ telefunc/server/runTelefunc.ts
@@ -93,7 +93,7 @@ async function runTelefunc_(runContext: {
     runContext.telefunctionName in runContext.telefunctions,
     `Could not find telefunction \`${
       runContext.telefunctionName
-    }\`. The client is likely out-of-sync with the server, see https://telefunc.com/out-of-sync. Loaded telefunctions: [${Object.keys(
+    }\`. The client is likely out-of-sync with the server, see https://telefunc.com/out-of-sync. Try reloading the client and/or server. Loaded telefunctions: [${Object.keys(
       runContext.telefunctions,
     ).join(', ')}]`,
   )
