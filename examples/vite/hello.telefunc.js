38d27368ef9d0d53c7fd773a31587c4103a76212
Romuald Brillout <git@brillout.com>
2022-02-02 20:35:09 +0100

    update example

--- examples/vite/hello.telefunc.js
+++ examples/vite/hello.telefunc.js
@@ -1,5 +1,9 @@
 export { hello }
 
+import { shield } from 'telefunc'
+const t = shield.type
+
+shield(hello, [{ name: t.string }])
 async function hello({ name }) {
   const message = 'Welcome ' + name
   return { message }
