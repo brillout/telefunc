35f8c684f4e3cf78218517dfee8d6cac427b2d8d
Mateo Hrastnik <mateo@lloyds.design>
2022-02-12 12:37:06 +0100

    Add React Native example

---
+++ examples/react-native/babel.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = function (api) {
+  api.cache(false);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      '../../telefunc/dist/node/babel/babel-plugin-telefunc.js',
+
+      // path.resolve(
+      //   __dirname,
+      //   '../../telefunc/dist/node/babel/babel-plugin-telefunc.js',
+      // ),
+    ],
+  };
+};
---
+++ examples/react-native/server/readPostList.telefunc.js
@@ -0,0 +1,3 @@
+export const readPostList = async () => {
+  return 'server-only-post';
+};
