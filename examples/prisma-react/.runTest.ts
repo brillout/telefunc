b2e596e27f8c319d4c55d35b6a6beb7aecd0d901
aslemammad <zorofight94@gmail.com>
2022-02-24 01:55:38 +0330

    better title

--- examples/prisma-react/.runTest.ts
+++ examples/prisma-react/.runTest.ts
@@ -1,4 +1,4 @@
-import { page, run, urlBase, autoRetry, fetchHtml } from '../../libframe/test/setup'
+import { page, run, urlBase, autoRetry } from '../../libframe/test/setup'
 
 export { runTest }
 
@@ -8,13 +8,13 @@ function runTest(cmd: 'npm run test:dev' | 'npm run test:prod') {
   test('Add to-do item', async () => {
     await page.goto(`${urlBase}/`)
     expect(await page.textContent('body')).toContain('todo list')
-    await page.fill('input[placeholder="Title"]', 'title')
-    await page.fill('input[placeholder="Content"]', 'content')
+    await page.fill('input[placeholder="Title"]', 'Star the telefunc repo')
+    await page.fill('input[placeholder="Content"]', 'the telefunc repo needs more stars')
     await page.click('button[type="submit"]')
 
     await autoRetry(async () => {
-      expect(await page.textContent('body')).toContain('title')
-      expect(await page.textContent('body')).toContain('content')
+      expect(await page.textContent('body')).toContain('Star the telefunc repo')
+      expect(await page.textContent('body')).toContain('the telefunc repo needs more stars')
     })
   })
 
@@ -23,8 +23,8 @@ function runTest(cmd: 'npm run test:dev' | 'npm run test:prod') {
     await autoRetry(async () => {
       const html = await page.content()
 
-      expect(html).toContain('title')
-      expect(html).toContain('content')
+      expect(html).toContain('Star the telefunc repo')
+      expect(html).toContain('the telefunc repo needs more stars')
     })
   })
 
