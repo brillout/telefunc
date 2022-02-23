13215f384646d9b236b7902df1a888778cc5aeb9
aslemammad <zorofight94@gmail.com>
2022-02-23 14:06:41 +0330

    type inference

--- examples/prisma-react/src/NewTodo.telefunc.ts
+++ examples/prisma-react/src/NewTodo.telefunc.ts
@@ -3,15 +3,15 @@ import prisma from './client'
 
 const t = shield.type
 
-export { onNewTodo }
-
-shield(onNewTodo, [{ title: t.string, content: t.string }])
-async function onNewTodo({ title, content }: { title: string; content: string }) {
-  await prisma.todo.create({
-    data: {
-      title,
-      content,
-      completed: false,
-    },
-  })
-}
+export const onNewTodo = shield(
+  [{ title: t.string, content: t.string }],
+  async function onNewTodo({ title, content }) {
+    await prisma.todo.create({
+      data: {
+        title,
+        content,
+        completed: false,
+      },
+    })
+  },
+)
