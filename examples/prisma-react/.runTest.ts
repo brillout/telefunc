73cbd7dacfd987bccebd5fbaa31f95289bd66a81
aslemammad <zorofight94@gmail.com>
2022-02-24 02:14:07 +0330

    remove unused

--- examples/prisma-react/.runTest.ts
+++ examples/prisma-react/.runTest.ts
@@ -1,4 +1,4 @@
-import { page, run, urlBase, autoRetry, fetchHtml } from '../../libframe/test/setup'
+import { page, run, urlBase, autoRetry } from '../../libframe/test/setup'
 
 export { runTest }
 
