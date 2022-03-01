d65dcc6674844d12db0874e52c8704cdd50ef56b
Mateo Hrastnik <mateo@lloyds.design>
2022-03-02 00:21:52 +0100

    Fix expo web

--- examples/react-native-expo/babel.config.js
+++ examples/react-native-expo/babel.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 module.exports = function (api) {
-  api.cache(true)
+  api.cache(true);
   return {
+    plugins: [path.resolve(__dirname, 'node_modules/telefunc/dist/node/babel/babel-plugin-telefunc')],
     presets: ['babel-preset-expo'],
-    plugins: ['telefunc/babel-plugin'],
-  }
-}
+  };
+};
