275722ea119a6fe89fc3cf3655d8d8c4cc633b33
Romuald Brillout <git@brillout.com>
2022-03-03 15:40:40 +0100

    remake client esm only

--- telefunc/client.js
+++ telefunc/client.js
@@ -1,4 +1,4 @@
 // Some tools don't support `package.json#exports`, such as:
 // - Nuxt v2
 // - Expo/Metro
-module.exports = require('./dist/client/index.js')
+export * from './dist/client/index.js'
