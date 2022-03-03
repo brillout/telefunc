816295d2429b69d856e3448db869f5e98a25d87c
Romuald Brillout <git@brillout.com>
2022-03-03 16:21:22 +0100

    move code

--- telefunc/node/transformer/transformTelefuncFile.ts
+++ telefunc/node/transformer/transformTelefuncFile.ts
@@ -1,25 +1,9 @@
 export { transformTelefuncFile }
 
-import { posix } from 'path'
-import { assert } from '../utils'
-import { assertPosixPath } from '../utils/assertPosixPath'
-import { getCode } from './getCode'
 import { getExportNames } from './getExportNames'
+import { transformTelefuncFileSync } from './transformTelefuncFileSync'
 
 async function transformTelefuncFile(src: string, id: string, root: string) {
-  assertPosixPath(id)
-  assertPosixPath(root)
-
-  const telefuncFilePath = '/' + posix.relative(root, id)
-  assert(!telefuncFilePath.startsWith('/.'))
-  assertPosixPath(telefuncFilePath)
-
   const exportNames = await getExportNames(src)
-
-  return {
-    code: getCode(exportNames, telefuncFilePath),
-    map: null,
-  }
+  return transformTelefuncFileSync(id, root, exportNames)
 }
-
-
