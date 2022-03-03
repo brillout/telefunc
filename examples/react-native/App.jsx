2ef214f457824e055d770f3a166c2990bd446e9e
Romuald Brillout <git@brillout.com>
2022-03-03 13:31:37 +0100

    polish

--- examples/react-native/App.jsx
+++ examples/react-native/App.jsx
@@ -1,38 +1,27 @@
 import { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
-import { readData } from './readData.telefunc.mjs'
-import { telefuncConfig } from 'telefunc/dist/client'
+import { hello } from './hello.telefunc.mjs'
+import { telefuncConfig } from 'telefunc/client'
 
-/**
- * Change this to `http://localhost:3000/_telefunc` if you're running the app
- * from a simulator or expo web, otherwise enter your computer's IP address.
- */
 telefuncConfig.telefuncUrl = 'http://localhost:3000/_telefunc'
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState(undefined)
-  const [error, setError] = useState(undefined)
-  useEffect(function effect() {
-    async function runEffect() {
-      setIsLoading(true)
+  const [state, set] = useState({ isLoading: true })
 
+  useEffect(() => {
+    (async () => {
       try {
-        const data = await readData()
-        setData(data)
+        const data = await hello({ name: 'Eva' })
+        set({ data })
       } catch (error) {
-        setError(String(error))
+        set({ error })
       }
-
-      setIsLoading(false)
-    }
-
-    runEffect()
+    })()
   }, [])
 
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text style={{ fontFamily: 'monospace' }}>{JSON.stringify({ isLoading, error, data }, null, 2)}</Text>
+      <Text style={{ fontFamily: 'monospace' }}>{JSON.stringify(state, null, 2)}</Text>
     </View>
   )
 }
