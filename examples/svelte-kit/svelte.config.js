72705092b46b45f7e070fec72a6e02eb0a069548
Patrick G <doeixd@gmail.com>
2022-03-04 15:24:59 -0500

    fixed formatting

--- examples/svelte-kit/svelte.config.js
+++ examples/svelte-kit/svelte.config.js
@@ -2,10 +2,10 @@ import adapter from '@sveltejs/adapter-auto'
 import {telefunc} from 'telefunc/vite'
 
 const config = {
-	adapter: adapter,
-	kit: {
-		vite: () => ({plugins: [telefunc()]})
-	}
+  adapter: adapter,
+  kit: {
+    vite: () => ({plugins: [telefunc()]})
+  }	
 }
 
 export default config
