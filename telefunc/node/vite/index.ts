0e70bd97f441030dc324fcbb1525d62b813541da
Romuald Brillout <git@brillout.com>
2022-03-08 08:56:27 +0100

    remove vite-plugin-import-build

--- telefunc/node/vite/index.ts
+++ telefunc/node/vite/index.ts
@@ -3,8 +3,6 @@ export default plugin
 
 import { transform } from './transform'
 import { build } from './build'
-// import { importBuild } from 'vite-plugin-import-build'
-// import { getImportBuildCode } from './getImportBuildCode'
 import { packageJsonFile } from './packageJsonFile'
 import { retrieveDevServer } from './retrieveDevServer'
 import type { Plugin } from 'vite'
@@ -27,7 +25,6 @@ function plugin(): Plugin[] {
     retrieveDevServer(),
     transform(),
     build(),
-    //importBuild(getImportBuildCode()),
     packageJsonFile(),
     distLinkOn()
   ]
