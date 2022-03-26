7d93d1f594a700b12ddf4af941c9a8895fdaba28
Bo <squ1sh@web.de>
2022-03-26 14:36:25 +0100

    move parcel-transformer-telefunc into an extra package

--- parcel-transformer-telefunc/transformer.ts
+++ parcel-transformer-telefunc/transformer.ts
@@ -1,15 +1,16 @@
 import {Transformer} from '@parcel/plugin';
-import {transformTelefuncFile} from "telefunc/node/transformer/transformTelefuncFile"
+
+//import {transformTelefuncFile} from "telefunc/transformer";
+const {transformTelefuncFile} = require("telefunc/transformer");
 
 export default new Transformer({
     async transform({asset}) {
-        // Retrieve the asset's source code and source map.
-        let source = await asset.getCode();
-        let sourceMap = await asset.getMap();
+
+        let sourceCode = await asset.getCode();
 
         // Run it through some compiler, and set the results
         // on the asset.
-        const { code } = await transformTelefuncFile(asset.filePath)
+        const { code } = await transformTelefuncFile(sourceCode)
 
         asset.setCode(code);
         asset.setMap(null);
