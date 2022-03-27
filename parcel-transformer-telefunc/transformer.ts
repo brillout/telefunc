0f15d9f1fc152889815116a0f122b2edc53b0cd2
Bo <squ1sh@web.de>
2022-03-27 23:29:18 +0200

    parcel transformer: fix: transformation resulted in an empty document.

--- parcel-transformer-telefunc/transformer.ts
+++ parcel-transformer-telefunc/transformer.ts
@@ -9,8 +9,7 @@ const {transformTelefuncFile} = require("telefunc/transformer");
 export default new Transformer({
     async transform({asset}) {
 
-        // Determine package path
-
+        // Determine package path:
         const packageJsonFile =await findPkg(dirname(asset.filePath));
         if(!packageJsonFile) {
             throw new Error("Cannot find package dir for file: " + asset.filePath + ". Make sure there's a package.json in one if its parent dirs.");
@@ -19,16 +18,15 @@ export default new Transformer({
         let packagePath = dirname(packageJsonFile);
         let sourceCode = await asset.getCode();
 
-
-        // Run it through some compiler, and set the results
-        // on the asset.
         const { code } = await transformTelefuncFile(sourceCode, asset.filePath, packagePath);
 
-        asset.setCode(code);
-        asset.setMap(null);
-
-        // Return the asset
-        return [asset];
+        // Return a new asset (must be a NEW asset, otherwise when modifying the current asset like in the example, it doesn't work somehow. Dit it like in @parcel/transformer-typescript-tsc/src/TSCTransformer.js)
+        return [{
+            type: 'ts',
+            filePath: asset.filePath,
+            content: code,
+            map: null,
+        }];
 
     }
 });
