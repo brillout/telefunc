e054ccf4783ef71582affd64d1d91cd4342fecb4
Bo <squ1sh@web.de>
2022-03-27 20:53:30 +0200

    Minor: added comments and rename params

--- telefunc/node/transformer/transformTelefuncFile.ts
+++ telefunc/node/transformer/transformTelefuncFile.ts
@@ -1,9 +1,16 @@
+// This API is used by parcel-transformer-telefunc package
 export { transformTelefuncFile }
 
 import { getExportNames } from './getExportNames'
 import { transformTelefuncFileSync } from './transformTelefuncFileSync'
 
-async function transformTelefuncFile(src: string, id: string, root: string) {
-  const exportNames = await getExportNames(src)
-  return transformTelefuncFileSync(id, root, exportNames)
+/**
+ *
+ * @param sourceCode
+ * @param filePath
+ * @param rootPath Path to the root
+ */
+async function transformTelefuncFile(sourceCode: string, filePath: string, rootPath: string) {
+  const exportNames = await getExportNames(sourceCode)
+  return transformTelefuncFileSync(filePath, rootPath, exportNames)
 }
