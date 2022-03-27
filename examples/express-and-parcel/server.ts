5c2750d5e5cf6f7034ef249a07912e14ca8054f3
Bo <squ1sh@web.de>
2022-03-27 20:51:42 +0200

    express-and-parcel: Added missing express integration code

--- examples/express-and-parcel/server.ts
+++ examples/express-and-parcel/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { telefunc } from "telefunc"
 
 const port = 5000;
 
@@ -9,6 +10,11 @@ const port = 5000;
     // Serve static pages:
     app.use(express.static('client/dist')); // TODO: only in production
 
+    app.all('/_telefunc', async (req, res) => {
+        const { originalUrl: url, method, body } = req
+        const httpResponse = await telefunc({ url, method, body })
+        res.status(httpResponse.statusCode).type(httpResponse.contentType).send(httpResponse.body)
+    })
     
 
     await app.listen(5000);
