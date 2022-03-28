d3616555141f48c7ee13fdf34ee07c611200ba44
Bo <squ1sh@web.de>
2022-03-28 15:09:36 +0200

    express-and-parcel: express integration code

--- examples/express-and-parcel/server.ts
+++ examples/express-and-parcel/server.ts
@@ -1,22 +1,28 @@
 import express from "express";
-import { telefunc } from "telefunc"
+import {telefunc, telefuncConfig} from "telefunc"
+import {provideTelefuncContext} from "telefunc";
 
 const port = 5000;
+
+telefuncConfig.telefuncFiles = [require.resolve("./hello.telefunc")];
 
 (async () => {
     const app = express();
 
 
-    // Serve static pages:
-    app.use(express.static('client/dist')); // TODO: only in production
+    // Serve static pages (for production use):
+    app.use(express.static('client/dist'));
 
+    // Telefunc integration:
+    app.use(express.text()) // Parse & make HTTP request body available at `req.body`
     app.all('/_telefunc', async (req, res) => {
-        const { originalUrl: url, method, body } = req
-        const httpResponse = await telefunc({ url, method, body })
-        res.status(httpResponse.statusCode).type(httpResponse.contentType).send(httpResponse.body)
+        // The usual Telefunc integration
+        const httpResponse = await telefunc({ url: req.url, method: req.method, body: req.body })
+        const { body, statusCode, contentType } = httpResponse
+        res.status(statusCode).type(contentType).send(body)
     })
     
 
     await app.listen(5000);
-    console.log("Server started: http://localhost:" + port)
+    console.log("Server started: http://localhost:" + port);
 })();
