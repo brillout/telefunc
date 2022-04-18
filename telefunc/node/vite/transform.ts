b71bea9bcc8d1cd11544fc38958be93aa7029617
Bart Louwers <bart@emeel.net>
2022-04-18 21:27:28 +0200

    Auto-generate shield() calls using template literal types, #23

--- telefunc/node/vite/transform.ts
+++ telefunc/node/vite/transform.ts
@@ -2,19 +2,28 @@ import { Plugin } from 'vite'
 import { assert, toPosixPath } from '../utils'
 import { transformTelefuncFile } from '../transformer/transformTelefuncFile'
 import { isSSR_options } from './utils'
+import { readFileSync } from 'fs'
+import { shieldTelefunctions } from '../server/shield/codegen/transformer'
 
 export { transform }
 
+// FIXME?
+const typesSrc = readFileSync(`${__dirname}/../server/shield/codegen/types.d.ts`).toString()
+
 function transform(): Plugin {
   let root: undefined | string
+
   return {
     name: 'telefunc:transform',
+    enforce: 'pre',
     config: (config) => {
       // Vite doesn't seem to always normalize config.root
       root = config.root ? toPosixPath(config.root) : toPosixPath(process.cwd())
     },
     async transform(src, id, options) {
       if (isSSR_options(options)) {
+        if (id.endsWith(".ts"))
+          return shieldTelefunctions(src, typesSrc)
         return
       }
       if (id.includes('.telefunc.')) {
