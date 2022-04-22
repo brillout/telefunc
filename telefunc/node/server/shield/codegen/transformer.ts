7c45138d350d77f3b9cb37417c7f6cfd5cb8f574
Romuald Brillout <git@brillout.com>
2022-04-22 07:49:49 +0200

    make variable names consistent with rest of telefunc src & remove outdated comment

--- telefunc/node/server/shield/codegen/transformer.ts
+++ telefunc/node/server/shield/codegen/transformer.ts
@@ -7,7 +7,7 @@ import { assert, assertWarning } from '../../../utils'
 const typesSrc = readFileSync(`${__dirname}/types.d.ts`).toString()
 
 const generateShield = (
-  telefuncSrc: string
+  telefuncFileCode: string
 ): string => {
   const project = new Project({
     compilerOptions: {
@@ -16,48 +16,45 @@ const generateShield = (
   })
 
   project.createSourceFile("types.ts", typesSrc)
-  const telefuncSourceFile = project.createSourceFile("telefunc.ts", telefuncSrc)
+  const telefuncFileSource = project.createSourceFile("telefunc.ts", telefuncFileCode)
   // this source file is used for evaluating the template literal types' values
-  const shieldStrSourceFile = project.createSourceFile("shield-str.ts")
+  const shieldStrSource = project.createSourceFile("shield-str.ts")
 
-  shieldStrSourceFile.addImportDeclaration({
+  shieldStrSource.addImportDeclaration({
     moduleSpecifier: "./types",
     namedImports: ["ShieldArrStr"]
   })
 
-  const teleFunctions = telefuncSourceFile.getFunctions().filter(fun => fun.isExported())
-  const teleFunNames = teleFunctions.flatMap(fun => {
-    const name = fun.getName()
+  const telefunctions = telefuncFileSource.getFunctions().filter(f => f.isExported())
+  const telefunctionNames = telefunctions.flatMap(telefunction => {
+    const name = telefunction.getName()
     if (!name) return []
     return [name]
   })
-  shieldStrSourceFile.addImportDeclaration({
+  shieldStrSource.addImportDeclaration({
     moduleSpecifier: "./telefunc",
-    namedImports: teleFunNames
+    namedImports: telefunctionNames
   })
 
   const tAlias = '__shieldGenerator_t'  // alias for shield.t
   // assign the template literal type to a string
   // then diagnostics are used to get the value of the template literal type
-  for (const teleFunName of teleFunNames) {
-    shieldStrSourceFile.addTypeAlias({
-      name: `${teleFunName}Shield`,
-      type: `ShieldArrStr<Parameters<typeof ${teleFunName}>>`
+  for (const telefunctionName of telefunctionNames) {
+    shieldStrSource.addTypeAlias({
+      name: `${telefunctionName}Shield`,
+      type: `ShieldArrStr<Parameters<typeof ${telefunctionName}>>`
     })
   }
 
   const shieldAlias = '__shieldGenerator_shield'  // alias for shield
-  // TODO: do users ever want to add shield() calls themselves?
-  // in that case we should detect if it has already been imported
-  // and skip auto-generating a shield() call
-  telefuncSourceFile.addImportDeclaration({
+  telefuncFileSource.addImportDeclaration({
     moduleSpecifier: 'telefunc',
     namedImports: [{
       name: 'shield',
       alias: shieldAlias
     }]
   })
-  telefuncSourceFile.addVariableStatement({
+  telefuncFileSource.addVariableStatement({
     declarationKind: VariableDeclarationKind.Const,
     declarations: [{
       name: tAlias,
@@ -65,19 +62,19 @@ const generateShield = (
     }]
   })
 
-  for (const teleFunName of teleFunNames) {
-    const typeAlias = shieldStrSourceFile.getTypeAlias(`${teleFunName}Shield`)
-    assert(typeAlias, `Failed to get typeAlias '${teleFunName}Shield'.`)
+  for (const telefunctionName of telefunctionNames) {
+    const typeAlias = shieldStrSource.getTypeAlias(`${telefunctionName}Shield`)
+    assert(typeAlias, `Failed to get typeAlias '${telefunctionName}Shield'.`)
 
     const shieldStr = typeAlias.getType().getLiteralValue()
 
     if (!shieldStr || typeof shieldStr !== 'string') {
-      assertWarning(false, `Failed to generate shield() for telefunction '${teleFunName}'`)
+      assertWarning(false, `Failed to generate shield() for telefunction ${telefunctionName}()`)
       continue
     }
     const shieldStrWithAlias = shieldStr.replace(/t\./g, `${tAlias}.`)
-    telefuncSourceFile.addStatements(`${shieldAlias}(${teleFunName}, ${shieldStrWithAlias}, { __generated: true })`)
+    telefuncFileSource.addStatements(`${shieldAlias}(${telefunctionName}, ${shieldStrWithAlias}, { __generated: true })`)
   }
 
-  return telefuncSourceFile.getText()
+  return telefuncFileSource.getText()
 }
