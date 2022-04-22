96da232db3cd183b26610214e018e15611a6b23b
Romuald Brillout <git@brillout.com>
2022-04-22 08:36:38 +0200

    add test for shield auto generation

--- examples/vite/.testRun.ts
+++ examples/vite/.testRun.ts
@@ -1,11 +1,11 @@
-import { page, run, urlBase, autoRetry, fetchHtml } from '../../libframe/test/setup'
+import { page, run, urlBase, autoRetry, fetchHtml, fetch } from '../../libframe/test/setup'
 
 export { testRun }
 
 function testRun(cmd: 'npm run dev' | 'npm run prod' | 'npm run start') {
   run(cmd)
 
-  test('Telefunc works', async () => {
+  test('example', async () => {
     {
       const html = await fetchHtml('/')
       expect(html).toContain('Loading...')
@@ -20,4 +20,30 @@ function testRun(cmd: 'npm run dev' | 'npm run prod' | 'npm run start') {
       expect(await page.textContent('body')).not.toContain('Loading')
     }
   })
+
+  test('shield() generation', async () => {
+    {
+      const resp = await makeTelefuncHttpRequest('Jon')
+      const { ret } = await resp.json()
+      expect(resp.status).toBe(200)
+      expect(ret.message).toBe('Welcome Jon')
+    }
+    {
+      const resp = await makeTelefuncHttpRequest(1337)
+      expect(resp.status).toBe(500)
+      expect(await resp.text()).toBe('Internal Server Error (Telefunc Request)')
+    }
+  })
+}
+
+async function makeTelefuncHttpRequest(name: string | number) {
+  const resp = await fetch(`${urlBase}/_telefunc`, {
+    method: 'POST',
+    body: JSON.stringify({
+      file: '/hello.telefunc.ts',
+      name: 'hello',
+      args: [{ name }]
+    })
+  })
+  return resp
 }
