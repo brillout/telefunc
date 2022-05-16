4c3c1636c07f57f542916f596ea9ce6c5a4d0848
Romuald Brillout <git@brillout.com>
2022-05-16 21:41:50 +0200

    fix: treat `Object.ceate(null)` as plain JavaScript object

--- telefunc/client/utils/isPlainObject.ts
+++ telefunc/client/utils/isPlainObject.ts
@@ -3,5 +3,17 @@ export { isPlainObject }
 type PlainObject = Record<string, unknown>
 
 function isPlainObject(value: unknown): value is PlainObject {
-  return typeof value === 'object' && value !== null && value.constructor === Object
+  if (typeof value !== 'object' || value === null) {
+    return false
+  }
+  // Support `Object.create(null)`
+  if (Object.getPrototypeOf(value) === null) {
+    return true
+  }
+  return (
+    /* Doesn't work in Cloudlfare Pages workers
+    value.constructor === Object
+    */
+    value.constructor.name === 'Object'
+  )
 }
