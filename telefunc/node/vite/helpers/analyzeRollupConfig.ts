7d4d014dcd6ae337ef3fdfad9d8bcbe59f0e3c58
Romuald Brillout <git@brillout.com>
2022-07-30 23:36:13 +0200

    remove unused code

--- telefunc/node/vite/helpers/analyzeRollupConfig.ts
+++ telefunc/node/vite/helpers/analyzeRollupConfig.ts
@@ -1,7 +1,7 @@
 export { analyzeRollupConfig }
 
 import type { ResolvedConfig } from 'vite'
-import { assert, assertPosixPath, assertUsage, isSSR_config } from '../utils'
+import { assert, isSSR_config } from '../utils'
 
 // Subset of: `import type { NormalizedOutputOptions } from 'rollup'` (to avoid mismatch upon different Rollup versions)
 type NormalizedOutputOptions = { entryFileNames: string | ((chunkInfo: any) => string); format: string }
@@ -15,26 +15,7 @@ type RollupResolved = {
 function analyzeRollupConfig(rollupResolved: RollupResolved, config: ResolvedConfig) {
   assert(isSSR_config(config))
   const isEsm = isEsmFormat(rollupResolved)
-  const fileExt = getFileExt(rollupResolved)
-  assert(fileExt !== 'mjs' || isEsm)
-  const pageFilesOutput = `pageFiles.${fileExt}`
-  {
-    const bundleFiles = Object.keys(rollupResolved.bundle)
-    const { outDir } = config.build
-    assertPosixPath(outDir)
-    assert(outDir.endsWith('/server'), { outDir })
-    assertUsage(
-      bundleFiles.includes(pageFilesOutput),
-      `\`${outDir}/${pageFilesOutput}\` is missing: make sure your Rollup config doesn't change the filename \`${pageFilesOutput}\``,
-    )
-  }
-  return { isEsm, pageFilesOutput }
-}
-
-function getFileExt(rollupResolved: { options: NormalizedOutputOptions; bundle: OutputBundle }): 'js' | 'mjs' {
-  const { entryFileNames } = rollupResolved.options
-  const fileExt = typeof entryFileNames === 'string' && entryFileNames.endsWith('.mjs') ? 'mjs' : 'js'
-  return fileExt
+  return { isEsm }
 }
 
 function isEsmFormat(rollupResolved: RollupResolved): boolean {
@@ -46,7 +27,7 @@ function isEsmFormat(rollupResolved: RollupResolved): boolean {
       format === 'es' ||
       format === 'iife' ||
       format === 'system' ||
-      format === 'umd',
+      format === 'umd'
   )
   return format === 'es'
 }
