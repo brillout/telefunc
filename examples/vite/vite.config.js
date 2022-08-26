a52a76794a08699983c782a52c21a838ba7403cd
Romuald Brillout <git@brillout.com>
2022-08-26 14:44:29 +0200

    use Vite appType mpa patch

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -1,11 +1,11 @@
 import { telefunc } from 'telefunc/vite'
 
+const port = 3000
+
 export default {
   plugins: [telefunc()],
   build: { target: 'esnext' },
   appType: 'mpa',
-  server: {
-    port: 3000,
-    host: true
-  }
+  server: { port, host: true },
+  preview: { port }
 }
