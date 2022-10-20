18d1a02b46e2acc7d03b77f46a4f7f0dd212e4f3
Romuald Brillout <git@brillout.com>
2022-10-20 09:38:07 +0200

    update test-e2e

---
+++ test-e2e.config.mjs
@@ -0,0 +1,55 @@
+export default {
+  ci: {
+    jobs: getCiJobs()
+  }
+}
+
+function getCiJobs() {
+  const ubuntu18 = {
+    os: 'ubuntu-latest',
+    node_version: '16'
+  }
+  const ubuntu16 = {
+    os: 'ubuntu-latest',
+    node_version: '16'
+  }
+  const ubuntu17 = {
+    os: 'ubuntu-latest',
+    node_version: '17'
+  }
+  const win14 = {
+    os: 'windows-latest',
+    node_version: '14'
+  }
+  const mac17 = {
+    os: 'macos-latest',
+    node_version: '17'
+  }
+
+  return [
+    {
+      name: 'Vite',
+      setups: [ubuntu18, win14, mac17]
+    },
+    {
+      name: 'React Native',
+      setups: [ubuntu16]
+    },
+    {
+      name: 'Cloudflare Workers',
+      setups: [ubuntu17]
+    },
+    {
+      name: 'Next.js',
+      setups: [ubuntu16, win14]
+    },
+    {
+      name: 'Nuxt 2',
+      setups: [ubuntu16, win14]
+    },
+    {
+      name: 'https://telefunc.com',
+      setups: [ubuntu18]
+    }
+  ]
+}
