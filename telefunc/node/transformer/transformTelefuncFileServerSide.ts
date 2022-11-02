f00daad74251b178cfdf6148603f03a23f6163c7
Romuald Brillout <git@brillout.com>
2022-11-02 09:55:08 +0100

    minor refactor

--- telefunc/node/transformer/transformTelefuncFileServerSide.ts
+++ telefunc/node/transformer/transformTelefuncFileServerSide.ts
@@ -1,7 +1,7 @@
 export { transformTelefuncFileServerSide }
 
 import { getExportNames } from './getExportNames'
-import { assert, assertPosixPath } from './utils'
+import { assertPosixPath } from './utils'
 import { generateShield } from '../server/shield/codegen/generateShield'
 
 async function transformTelefuncFileServerSide(
@@ -15,13 +15,10 @@ async function transformTelefuncFileServerSide(
   assertPosixPath(root)
 
   const exportNames = await getExportNames(src)
-
   let code = decorateTelefunctions(exportNames, src, id.replace(root, ''), root, skipRegistration)
 
-  if (!isDev) {
-    if (id.endsWith('.ts')) {
-      code = generateShield(code, id)
-    }
+  if (id.endsWith('.ts') && !isDev) {
+    code = generateShield(code, id)
   }
 
   return code
@@ -36,21 +33,19 @@ function decorateTelefunctions(
 ) {
   assertPosixPath(filePath)
 
-  const codePreprend: string = (() => {
-    let line = 'import { __decorateTelefunction } from "telefunc";'
-    return line
-  })()
-
-  const codeAppend: string = (() => {
-    const lines: string[] = []
-    for (const exportName of exportNames) {
-      lines.push(`__decorateTelefunction(${exportName}, "${exportName}", "${filePath}", "${appRootDir}", ${String(skipRegistration)});`)
-    }
-    return lines.join('\n')
-  })()
-
-  // No break line before `src` to avoid breaking the source map
-  assert(!codePreprend.includes('\n'))
-  const code = `${codePreprend}${src}\n\n${codeAppend}\n`
-  return code
+  return [
+    'import { __decorateTelefunction } from "telefunc";',
+    // No break line before `src` to avoid breaking source map lines
+    src,
+    '\n\n',
+    exportNames
+      .map(
+        (exportName) =>
+          `__decorateTelefunction(${exportName}, "${exportName}", "${filePath}", "${appRootDir}", ${String(
+            skipRegistration
+          )});`
+      )
+      .join('\n'),
+    '\n'
+  ].join('')
 }
