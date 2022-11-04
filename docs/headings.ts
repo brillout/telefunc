c759e9181310c896a3152260392926f3f90ef657
Romuald Brillout <git@brillout.com>
2022-11-04 18:44:58 +0100

    docs: add API docs

--- docs/headings.ts
+++ docs/headings.ts
@@ -18,6 +18,10 @@ const headingsWithoutLink: HeadingWithoutLink[] = [
   {
     title: 'Server-Side Rendering (SSR)',
     url: '/ssr'
+  },
+  {
+    title: 'Telefunc Transformer',
+    url: '/transformer'
   }
 ]
 
@@ -57,6 +61,11 @@ export const headings: HeadingDefinition[] = [
     level: 2,
     title: 'Nuxt',
     url: '/nuxt'
+  },
+  {
+    level: 2,
+    title: 'React Native',
+    url: '/react-native'
   },
   {
     level: 2,
@@ -102,6 +111,15 @@ export const headings: HeadingDefinition[] = [
   },
   {
     level: 4,
+    title: 'Context'
+  },
+  {
+    level: 2,
+    title: '`getContext()`',
+    url: '/getContext'
+  },
+  {
+    level: 4,
     title: 'Protection'
   },
   {
@@ -112,29 +130,78 @@ export const headings: HeadingDefinition[] = [
   {
     level: 2,
     title: '`shield()`',
-    url: '/shield'
+    url: '/shield',
+    sectionTitles: ['TypeScript']
   },
   {
     level: 4,
-    title: 'Context'
+    title: 'Server Middleware'
   },
   {
     level: 2,
-    title: '`getContext()`',
-    url: '/getContext'
+    title: '`telefunc()',
+    url: '/telefunc'
   },
   {
     level: 4,
-    title: 'Integration'
+    title: 'Error Handling'
   },
   {
     level: 2,
-    title: '`createTelefuncCaller()`',
-    url: '/createTelefuncCaller'
+    title: '`onBug()`',
+    url: '/onBug'
   },
   {
     level: 2,
-    title: '`config',
-    url: '/config'
-  }
+    title: '`onAbort()`',
+    url: '/onAbort'
+  },
+  {
+    level: 4,
+    title: 'Config'
+  },
+  {
+    level: 2,
+    title: '`telefuncUrl`',
+    url: '/telefuncUrl'
+  },
+  {
+    level: 2,
+    title: '`disableNamingConvention`',
+    url: '/disableNamingConvention'
+  },
+  {
+    level: 2,
+    title: '`httpHeaders',
+    url: '/httpHeaders'
+  },
+  {
+    level: 2,
+    title: '`telefuncFiles',
+    url: '/telefuncFiles'
+  },
+  {
+    level: 2,
+    title: '`root`',
+    url: '/root'
+  },
+  {
+    level: 4,
+    title: 'Plugins'
+  },
+  {
+    level: 2,
+    title: 'Vite Plugin',
+    url: '/vite-plugin'
+  },
+  {
+    level: 2,
+    title: 'Webpack Plugin',
+    url: '/webpack-plugin'
+  },
+  {
+    level: 2,
+    title: 'Babel Plugin',
+    url: '/babel-plugin'
+  },
 ]
