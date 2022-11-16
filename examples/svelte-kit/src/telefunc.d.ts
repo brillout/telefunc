b931d10e6e76f390d53f96914278437cf6bcdd2a
Romuald Brillout <git@brillout.com>
2022-11-16 19:13:43 +0100

    minor

--- examples/svelte-kit/src/telefunc.d.ts
+++ examples/svelte-kit/src/telefunc.d.ts
@@ -4,9 +4,9 @@ declare module 'telefunc' {
 	namespace Telefunc {
 		interface Context {
 			/* Globally define the type of the `context` object here, see https://telefunc.com/getContext#typescript
-       * For example:
-      user: null | { id: number, name: string }
-      */
+			 * For example:
+			user: null | { id: number, name: string }
+			*/
 		}
 	}
 }
