7178fecec8e08c55b1f9057d8c9fb1563698be27
Romuald Brillout <git@brillout.com>
2022-11-16 19:17:18 +0100

    add telefunc

--- examples/svelte-kit/src/routes/+page.svelte
+++ examples/svelte-kit/src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+	import Counter from './Counter.svelte';
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p><Counter count={data.value} /></p>
+<p>Refresh the page and observe that the counter value is preserved.</p>
---
+++ examples/svelte-kit/src/telefunc.d.ts
@@ -0,0 +1,12 @@
+import 'telefunc';
+
+declare module 'telefunc' {
+	namespace Telefunc {
+		interface Context {
+			/* Globally define the type of the `context` object here, see https://telefunc.com/getContext#typescript
+			 * For example:
+			user: null | { id: number, name: string }
+			*/
+		}
+	}
+}
