5867cc4bf78c5c4d51380cd158bcae64751fec41
Romuald Brillout <git@brillout.com>
2023-02-07 12:13:55 +0100

    improve SvelteKit example

--- examples/svelte-kit/src/routes/+page.svelte
+++ examples/svelte-kit/src/routes/+page.svelte
@@ -7,4 +7,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p><Counter count={data.value} /></p>
-<p>Refresh the page and observe that the counter value is preserved.</p>
+<p>
+	The counter value comes from the backend: refresh the page and observe that the counter value is
+	preserved. We use Telefunc to communicate between the frontend and the backend.
+</p>
