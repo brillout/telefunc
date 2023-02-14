e5b8c5249b3df4bf470e5c817c48c6468be89814
Romuald Brillout <git@brillout.com>
2023-02-14 01:20:13 +0100

    minor

--- telefunc/utils/isVitePluginSsr.ts
+++ telefunc/utils/isVitePluginSsr.ts
@@ -1,7 +1,7 @@
 export function isVitePluginSsr(): boolean {
+  // https://github.com/brillout/vite-plugin-ssr/blob/9d3cc76de9c966a9c38803553ecfcae740a02860/vite-plugin-ssr/node/index.ts#L16-L20
   return globalThis._isVitePluginSsr === true
 }
-
 declare global {
   var _isVitePluginSsr: undefined | boolean
 }
--- telefunc/utils/isWebpack.ts
+++ telefunc/utils/isWebpack.ts
@@ -6,7 +6,7 @@ function isWebpack(): boolean {
   const test1 = typeof __non_webpack_require__ === 'function'
   const test2 = typeof __webpack_require__ === 'function'
   assert(test1 === test2)
-  return test1! || test2
+  return test1 || test2
 }
 
 declare global {
