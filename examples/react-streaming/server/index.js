7991d7ec6f023d1985e1e77e1fbad7e1b88a1902
Romuald Brillout <git@brillout.com>
2023-02-25 18:10:10 +0100

    minor

--- examples/react-streaming/server/index.js
+++ examples/react-streaming/server/index.js
@@ -23,17 +23,9 @@ async function startServer() {
     app.use(viteDevMiddleware)
   }
 
-  app.use(function (req, _res, next) {
-    req.user = {
-      id: 0,
-      name: 'Elisabeth'
-    }
-    next()
-  })
-
   app.use(express.text()) // Parse & make HTTP request body available at `req.body`
   app.all('/_telefunc', async (req, res) => {
-    const context = { user: req.user }
+    const context = {}
     const httpResponse = await telefunc({ url: req.originalUrl, method: req.method, body: req.body, context })
     const { body, statusCode, contentType } = httpResponse
     res.status(statusCode).type(contentType).send(body)
@@ -41,7 +33,6 @@ async function startServer() {
 
   app.get('*', async (req, res, next) => {
     const pageContextInit = {
-      user: req.user,
       urlOriginal: req.originalUrl
     }
     const pageContext = await renderPage(pageContextInit)
