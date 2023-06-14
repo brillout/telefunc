3910b188e7347bf29cb5b019ad59ed78e15ecfec
Romuald Brillout <git@brillout.com>
2023-06-14 10:01:19 +0200

    fix: remove problematic filePath assert()

--- telefunc/utils/getTelefunctionKey.ts
+++ telefunc/utils/getTelefunctionKey.ts
@@ -1,7 +1,8 @@
-import { assert } from './assert'
-
 export function getTelefunctionKey(filePath: string, exportName: string) {
-  assert(!filePath.includes(':')) // This assert isn't actually necessary since `telefunctionKey` is never parsed
+  /* This assert fails for Filesystem Routing paths such as `pages/product/:id/index.js`.
+   * This assert isn't actually necessary since `telefunctionKey` is never parsed.
+  assert(!filePath.includes(':'))
+  */
   const telefunctionKey = filePath + ':' + exportName
   return telefunctionKey
 }
