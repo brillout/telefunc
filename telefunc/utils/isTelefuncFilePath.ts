b9d7d0ccb69f723db4ead79dbef440a8c0a8caf6
Romuald Brillout <git@brillout.com>
2023-09-21 12:23:55 +0200

    fix: remove assertion that fails on windows (#90)

--- telefunc/utils/isTelefuncFilePath.ts
+++ telefunc/utils/isTelefuncFilePath.ts
@@ -5,6 +5,5 @@ import { assert } from './assert'
 
 function isTelefuncFilePath(filePath: string): boolean {
   assertPosixPath(filePath)
-  assert(filePath.startsWith('/'))
   return filePath.includes('.telefunc.')
 }
