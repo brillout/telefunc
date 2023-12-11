3765c5659ad46fe31ea0817f3a65c9a72924ba12
Romuald Brillout <git@brillout.com>
2023-12-11 16:38:05 +0100

    fix: make standalone builds easier (vikejs/vike#1165, vikejs/vike#1342)

--- telefunc/node/vite/plugins/importBuild/index.ts
+++ telefunc/node/vite/plugins/importBuild/index.ts
@@ -28,16 +28,8 @@ function importBuild(): Plugin[] {
 }
 
 function getImporterCode(config: ResolvedConfig, telefuncFilesEntry: string) {
-  // Current file: node_modules/telefunc/dist/cjs/node/vite/plugins/importBuild/index.js
-  const importPathAbsolute = toPosixPath(
-    require.resolve(`../../../../../../dist/cjs/node/vite/plugins/importBuild/loadBuild.js`)
-  )
+  const importPath = getImportPath(config)
 
-  assert(config.build.ssr) // outDir needs to be the outDir of the server-side
-  const outDir = getOutDirAbsolute(config)
-  assertPosixPath(outDir)
-  assertPosixPath(importPathAbsolute)
-  const importPath = path.posix.relative(outDir, importPathAbsolute)
   // console.log(`\n  importPath: ${importPath}\n  outDirServer: ${outDirServer}\n  importPathAbsolute: ${importPathAbsolute}\n  config.build.outDir: ${config.build.outDir}`)
   const importerCode = [
     '{',
@@ -50,4 +42,25 @@ function getImporterCode(config: ResolvedConfig, telefuncFilesEntry: string) {
     ''
   ].join('\n')
   return importerCode
+}
+
+function getImportPath(config: ResolvedConfig) {
+  // We resolve filePathAbsolute even if we don't use it: we use require.resolve() as an assertion that the relative path is correct
+  const filePathAbsolute = toPosixPath(
+    // [RELATIVE_PATH_FROM_DIST] Current file: node_modules/telefunc/dist/cjs/node/vite/plugins/importBuild/index.js
+    require.resolve(`../../../../../../dist/cjs/node/vite/plugins/importBuild/loadBuild.js`)
+  )
+  if (
+    // Let's implement a new config if a user needs the import to be a relative path instead of 'telefunc/__internal/loadImportBuild' (AFAIK there is no use case for relative paths for Telefunc)
+    true as boolean
+  ) {
+    return 'telefunc/__internal/loadBuild'
+  } else {
+    assert(config.build.ssr) // outDir needs to be the outDir of the server-side
+    const outDir = getOutDirAbsolute(config)
+    assertPosixPath(outDir)
+    assertPosixPath(filePathAbsolute)
+    const filePathRelative = path.posix.relative(outDir, filePathAbsolute)
+    return filePathRelative
+  }
 }
