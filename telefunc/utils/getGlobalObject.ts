e6e82838c399942698e03d85ada54ffa81599a5c
Romuald Brillout <git@brillout.com>
2024-01-06 15:12:34 +0100

    minor refactor

--- telefunc/utils/getGlobalObject.ts
+++ telefunc/utils/getGlobalObject.ts
@@ -1,12 +1,11 @@
 export function getGlobalObject<T extends Record<string, unknown> = never>(
-  /** We use the filename or file path as module key */
-  moduleKey: `${string}.ts`,
+  // We use the filename as key; each `getGlobalObject()` call should live inside a file with a unique filename.
+  key: `${string}.ts`,
   defaultValue: T
 ): T {
-  const allGlobalObjects = (globalThis.__telefunc = globalThis.__telefunc || {})
-  const globalObject = (allGlobalObjects[moduleKey] = (allGlobalObjects[moduleKey] as T) || defaultValue)
+  // @ts-ignore
+  const globalObjectsAll = (globalThis[projectKey] = globalThis[projectKey] || {})
+  const globalObject = (globalObjectsAll[key] = globalObjectsAll[key] || defaultValue)
   return globalObject
 }
-declare global {
-  var __telefunc: undefined | Record<string, Record<string, unknown>>
-}
+const projectKey = '_telefunc'
