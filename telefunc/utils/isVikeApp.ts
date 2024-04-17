8b052ccfe6e50cef0808a62ed8815d9cc69204f9
Romuald Brillout <git@brillout.com>
2024-04-17 17:21:23 +0200

    comment

--- telefunc/utils/isVikeApp.ts
+++ telefunc/utils/isVikeApp.ts
@@ -1,5 +1,5 @@
 export function isVikeApp(): boolean {
   const g = globalThis as Record<string, unknown>
-  // Set by Vike: https://github.com/vikejs/vike/blob/c6fda92e838f1ea35f5df5a84f8e19781cdb81cf/vike/node/runtime/index-common.ts#L20
+  // Set by Vike: https://github.com/vikejs/vike/blob/8fa3acac956a8b310f8c7cbfa75e28bb42a93f7c/vike/node/runtime/index-common.ts#L20
   return !!g._isVikeApp || !!g._isVitePluginSsr
 }
