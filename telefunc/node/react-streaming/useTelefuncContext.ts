14008d77c6ee6dd58c1c7221e466c4182328c8a4
Romuald Brillout <git@brillout.com>
2024-05-27 10:53:28 +0200

    fix: avoid @types/react version mismatch

--- telefunc/node/react-streaming/useTelefuncContext.ts
+++ telefunc/node/react-streaming/useTelefuncContext.ts
@@ -7,13 +7,20 @@ import { assertUsage, isObject } from '../utils'
 
 const TelefuncReactContext = createContext<null | Telefunc.Context>(null)
 
-function TelefuncSSR({ context, children }: { context?: Telefunc.Context; children: ReactNode }) {
+function TelefuncSSR({
+  context,
+  children,
+}: {
+  context?: Telefunc.Context
+  // Using type `unknown` instead of type `ReactNode` in order to avoid mismatch when user has a different @types/react version.
+  children: unknown
+}) {
   assertUsage(
     context === undefined || isObject(context),
     '[<TelefuncSSR context={context}/>] context should be an object',
   )
   const { Provider } = TelefuncReactContext
-  return createElement(Provider, { value: context ?? null, children })
+  return createElement(Provider, { value: context ?? null, children: children as ReactNode })
 }
 
 function useTelefuncContext() {
