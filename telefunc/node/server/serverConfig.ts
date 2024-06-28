8127d2fa5afe777a4a7cf4501ec38d8cefe18007
Romuald Brillout <git@brillout.com>
2024-06-28 12:30:30 +0200

    minor refactor

--- telefunc/node/server/serverConfig.ts
+++ telefunc/node/server/serverConfig.ts
@@ -91,7 +91,7 @@ function validateUserConfig(configUserUnwrapped: ConfigUser, prop: string, val: 
   } else if (prop === 'shield') {
     assertUsage(typeof val === 'object' && val !== null, 'config.shield should be a object')
     if ('dev' in val) {
-      assertUsage(typeof val?.dev === 'boolean', 'config.shield.dev should be a boolean')
+      assertUsage(typeof val.dev === 'boolean', 'config.shield.dev should be a boolean')
     }
     configUserUnwrapped[prop] = val
   } else {
