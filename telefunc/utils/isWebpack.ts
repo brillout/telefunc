1e6403e4b8437d3da49bc3bbab5747749f0a7e68
Romuald Brillout <git@brillout.com>
2024-09-07 10:09:29 +0200

    fix: improve isWebpack() test (fix #121)

--- telefunc/utils/isWebpack.ts
+++ telefunc/utils/isWebpack.ts
@@ -1,11 +1,6 @@
-export { isWebpack }
-
-import { assert } from './assert'
-
-function isWebpack(): boolean {
+export function isWebpack(): boolean {
   const test1 = typeof __non_webpack_require__ === 'function'
   const test2 = typeof __webpack_require__ === 'function'
-  assert(test1 === test2)
   return test1 || test2
 }
 
