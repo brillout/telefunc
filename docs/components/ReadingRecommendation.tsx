1f77e2d2a21369041c8893e24ee9e5653ad9ab7e
Romuald Brillout <git@brillout.com>
2024-11-13 11:05:46 +0100

    eject `<ReadingRecommendation>`

---
+++ docs/components/ReadingRecommendation.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Link } from '@brillout/docpress'
+
+export { ReadingRecommendation }
+
+// TODO/eventually: remove
+function ReadingRecommendation({ tour, links }: { tour?: true; links?: string[] }) {
+  links ??= []
+  if (!links.length) tour = true
+  const multiple = links.length + (tour ? 1 : 0) > 1
+  const intro = <b>Reading Recommendation{multiple ? '.' : ': '}</b>
+  return (
+    <blockquote>
+      {(() => {
+        if (!multiple) {
+          const link = tour ? <TourLink /> : <Link href={links[0]} />
+          return (
+            <p>
+              {intro}
+              {link}
+            </p>
+          )
+        }
+        return (
+          <>
+            <p> {intro}</p>
+            <ul
+              style={{
+                marginLeft: 18,
+                marginTop: 11,
+              }}
+            >
+              {tour && (
+                <li>
+                  <TourLink />
+                </li>
+              )}
+              {links.map((link, i) => (
+                <li key={i}>
+                  <Link href={link} />
+                </li>
+              ))}
+            </ul>
+          </>
+        )
+      })()}
+    </blockquote>
+  )
+}
+
+function TourLink() {
+  return (
+    <>
+      <Link href={'/react-tour'} noBreadcrumb={true} /> or <Link href={'/vue-tour'} noBreadcrumb={true} />
+    </>
+  )
+}
