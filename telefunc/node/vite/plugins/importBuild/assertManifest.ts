981b6a2d5406ce5d797d2ecf014a11a697d799b1
Romuald Brillout <git@brillout.com>
2025-01-09 15:51:50 +0100

    fix: update error message

--- telefunc/node/vite/plugins/importBuild/assertManifest.ts
+++ telefunc/node/vite/plugins/importBuild/assertManifest.ts
@@ -9,7 +9,7 @@ function assertManifest(manifest: Record<string, unknown>): asserts manifest is 
   assert(hasProp(manifest, 'version', 'string'))
   assertUsage(
     manifest.version === projectInfo.projectVersion,
-    `Re-build your app (\`$ vite build\`). (You are using \`telefunc@${projectInfo.projectVersion}\` while your build has been generated with a different version \`telefunc@${manifest.version}\`.)`,
+    `Re-build your app. (You are using \`telefunc@${projectInfo.projectVersion}\` while your build has been generated with a different version \`telefunc@${manifest.version}\`.)`,
   )
   assert(hasProp(manifest, 'config', 'object'))
   checkType<Manifest>(manifest)
