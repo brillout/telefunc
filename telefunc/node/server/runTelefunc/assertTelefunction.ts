20ec17b31ce6795e23fee793901f4d2454cd85d4
Romuald Brillout <git@brillout.com>
2025-01-31 22:54:32 +0100

    fix: colorize error message

--- telefunc/node/server/runTelefunc/assertTelefunction.ts
+++ telefunc/node/server/runTelefunc/assertTelefunction.ts
@@ -2,11 +2,15 @@ export { assertTelefunction }
 
 import { assertUsage, isCallable } from '../../utils'
 import type { Telefunction } from '../types'
+import pc from '@brillout/picocolors'
 
 function assertTelefunction(
   exportValue: unknown,
   exportName: string,
   telefuncFilePath: string,
 ): asserts exportValue is Telefunction {
-  assertUsage(isCallable(exportValue), `\`export { ${exportName} }\` of ${telefuncFilePath} should be a function`)
+  assertUsage(
+    isCallable(exportValue),
+    `${pc.code(`export { ${exportName} }`)} of ${pc.bold(telefuncFilePath)} should be a function`,
+  )
 }
