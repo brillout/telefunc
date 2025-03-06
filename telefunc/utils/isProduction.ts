395e0ef499ec2496c95e4f7f39c0eee5b5f3c0b4
samuba <samuel.z.bach@gmail.com>
2025-03-06 15:55:44 +0100

    cache process.env calls

--- telefunc/utils/isProduction.ts
+++ telefunc/utils/isProduction.ts
@@ -8,10 +8,21 @@ function isProduction(): boolean {
   // We consider production if `val` is 'production', 'staging', 'test', etc.
   return true
 }
+
+// caching calls to process.env because it's expensive
+let _nodeEnv: null | undefined | string
 function getNodeEnv(): null | undefined | string {
-  if (isNotNode()) return null
-  return process.env.NODE_ENV
+  if (_nodeEnv === undefined) {
+    if (isNotNode()) return null
+    _nodeEnv = process.env.NODE_ENV
+  }
+  return _nodeEnv
 }
+
+let _isNotNode: undefined | boolean
 function isNotNode() {
-  return typeof process == 'undefined' || !('env' in process)
+  if (_isNotNode === undefined) {
+    _isNotNode = typeof process == 'undefined' || !('env' in process)
+  }
+  return _isNotNode
 }
