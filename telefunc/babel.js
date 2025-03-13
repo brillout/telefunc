474a7a73fc7058d0ee3a8e285c8de681fa8b5c07
Romuald Brillout <git@brillout.com>
2025-03-13 22:37:20 +0100

    update babel re-export

--- telefunc/babel.js
+++ telefunc/babel.js
@@ -1,2 +1,3 @@
 // React Native Expo/Metro doesn't seem to support `package.json#exports`.
-module.exports = require('./dist/esm/node/babel/plugin.js')
+export * from './dist/esm/node/babel/plugin.js'
+export { default } from './dist/esm/node/babel/plugin.js'
