c01a95903e66a9543ba481651f48bad01f8ef60c
Romuald Brillout <git@brillout.com>
2025-04-01 11:53:03 +0200

    use magic-string

--- telefunc/node/transformer/transformTelefuncFileServerSide.ts
+++ telefunc/node/transformer/transformTelefuncFileServerSide.ts
@@ -1,9 +1,10 @@
 export { transformTelefuncFileServerSide }
 
 import { ExportList, getExportList } from './getExportList.js'
-import { assertPosixPath } from './utils.js'
+import { assertPosixPath, isNotNullish } from './utils.js'
 import { generateShield } from './generateShield/generateShield.js'
 import { getServerConfig } from '../server/serverConfig.js'
+import MagicString from 'magic-string'
 
 async function transformTelefuncFileServerSide(
   src: string,
@@ -16,19 +17,24 @@ async function transformTelefuncFileServerSide(
   assertPosixPath(appRootDir)
 
   const exportList = await getExportList(src)
-  let code = decorateTelefunctions(exportList, src, id.replace(appRootDir, ''), appRootDir, skipRegistration)
+  const codeDecoration = decorateTelefunctions(exportList, id.replace(appRootDir, ''), appRootDir, skipRegistration)
 
+  let codeShield: string | undefined
   const config = getServerConfig()
   if (id.endsWith('.ts') && (!isDev || config.shield.dev)) {
-    code += '\n\n' + generateShield(code, id, appRootDir, exportList)
+    codeShield = generateShield(src, id, appRootDir, exportList)
   }
 
-  return { code, map: undefined }
+  const s = new MagicString(src)
+  // We append everything in order to avoid breaking source map lines for environments that don't support source maps
+  s.append(['', codeDecoration, codeShield].filter(isNotNullish).join('\n\n'))
+  const code = s.toString()
+  const map = s.generateMap({ hires: true, source: id })
+  return { code, map }
 }
 
 function decorateTelefunctions(
   exportList: ExportList,
-  src: string,
   filePath: string,
   appRootDir: string,
   skipRegistration: boolean,
@@ -36,18 +42,12 @@ function decorateTelefunctions(
   assertPosixPath(filePath)
 
   return [
-    // We append everything in order to avoid breaking source map lines for environments that don't support source maps
-    src,
-    '\n\n',
     'import { __decorateTelefunction } from "telefunc";',
-    exportList
-      .map(
-        ({ exportName, localName }) =>
-          `__decorateTelefunction(${localName || exportName}, "${exportName}", "${filePath}", "${appRootDir}", ${String(
-            skipRegistration,
-          )});`,
-      )
-      .join('\n'),
-    '\n',
-  ].join('')
+    ...exportList.map(
+      ({ exportName, localName }) =>
+        `__decorateTelefunction(${localName || exportName}, "${exportName}", "${filePath}", "${appRootDir}", ${String(
+          skipRegistration,
+        )});`,
+    ),
+  ].join('\n')
 }
