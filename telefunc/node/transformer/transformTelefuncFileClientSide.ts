816a1fd560e5914e7c4bc241c1112cbdf65a0266
Romuald Brillout <git@brillout.com>
2025-04-04 14:15:59 +0200

    minor refactor

--- telefunc/node/transformer/transformTelefuncFileClientSide.ts
+++ telefunc/node/transformer/transformTelefuncFileClientSide.ts
@@ -1,12 +1,10 @@
 export { transformTelefuncFileClientSide }
 
-import { rollupSourceMapRemove } from '../vite/utils.js'
 import { getExportList } from './getExportList.js'
 import { transformTelefuncFileClientSideSync } from './transformTelefuncFileClientSideSync.js'
 
 async function transformTelefuncFileClientSide(src: string, id: string, appRootDir: string) {
   const exportList = await getExportList(src)
   const exportNames = exportList.map((e) => e.exportName)
-  const { code } = transformTelefuncFileClientSideSync(id, appRootDir, exportNames)
-  return rollupSourceMapRemove(code)
+  return transformTelefuncFileClientSideSync(id, appRootDir, exportNames)
 }
