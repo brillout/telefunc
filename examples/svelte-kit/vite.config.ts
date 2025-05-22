8f633da7e50030cb8d4b6d0c2f7e61779f8ebcfe
Romuald Brillout <git@brillout.com>
2025-05-22 12:09:27 +0200

    adjust scaffolded app

--- examples/svelte-kit/vite.config.ts
+++ examples/svelte-kit/vite.config.ts
@@ -3,4 +3,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [sveltekit()],
+  // Avoid page reload which breaks the CI
+  optimizeDeps: { include: ['clsx', 'devalue'] },
 })
