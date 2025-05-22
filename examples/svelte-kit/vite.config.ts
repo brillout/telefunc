f1b17f1684d0dd04756d1a0c71b6883fad6ec660
Romuald Brillout <git@brillout.com>
2025-05-22 11:35:10 +0200

    rm examples/svelte-kit/

--- examples/svelte-kit/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import type { UserConfig } from 'vite'
-import { telefunc } from 'telefunc/vite'
-
-const config: UserConfig = {
-  plugins: [sveltekit(), telefunc()],
-}
-
-export default config
