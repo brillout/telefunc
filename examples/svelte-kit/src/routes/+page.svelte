d31fc028135820281141e75a5f86025bbc0a47c1
Romuald Brillout <git@brillout.com>
2025-05-22 12:27:35 +0200

    showcase telefunc

--- examples/svelte-kit/src/routes/+page.svelte
+++ examples/svelte-kit/src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+import Counter from './Counter.svelte'
+/** @type {import('./$types').PageData} */
+export let data
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Counter count={data.value} />
+<p>
+	The counter value comes from the backend: refresh the page and observe that the counter value is
+	preserved. We use Telefunc to communicate between the frontend and the backend.
+</p>
--- examples/svelte-kit/vite.config.ts
+++ examples/svelte-kit/vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import { telefunc } from 'telefunc/vite'
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), telefunc()],
   // Avoid page reload which breaks the CI
   optimizeDeps: { include: ['clsx', 'devalue'] },
 })
