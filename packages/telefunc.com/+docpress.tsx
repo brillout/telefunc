8178657409fcc941604abf969471d9da35f82fef
Romuald Brillout <git@brillout.com>
2025-06-20 10:11:01 +0200

    update +docpress

--- packages/telefunc.com/+docpress.tsx
+++ packages/telefunc.com/+docpress.tsx
@@ -1,32 +1,33 @@
 export { config as default }
 
 import type { Config } from '@brillout/docpress'
-import logoUrl from './icons/telefunc.svg'
+import logo from './icons/telefunc.svg'
 import { categories, headings, headingsDetached } from './headings'
 import { PROJECT_VERSION } from './PROJECT_VERSION.js'
 import { TopNavigation } from './TopNavigation'
 import React from 'react'
 
 const config: Config = {
-  projectInfo: {
-    projectName: 'Telefunc',
-    projectVersion: PROJECT_VERSION,
-    githubRepository: 'https://github.com/brillout/telefunc',
-    githubIssues: 'https://github.com/brillout/telefunc/issues/new',
-    twitterProfile: 'https://twitter.com/brillout',
-  },
-  docsDir: 'packages/telefunc.com',
-  logoUrl,
+  name: 'Telefunc',
+  version: PROJECT_VERSION,
+  url: 'https://telefunc.com',
+  tagline: 'Remote Functions. Instead of API.',
+  logo,
+
+  github: 'https://github.com/brillout/telefunc',
+  twitter: '@brillout',
+
   headings,
   headingsDetached,
   categories,
-  topNavigation: <TopNavigation />,
-  tagline: 'Remote Functions. Instead of API.',
-  twitterHandle: '@brillout',
-  websiteUrl: 'https://telefunc.com',
+
   algolia: {
     appId: 'NONXS2JSTL',
     apiKey: '9bf6a6f9bc168ca425e8e19a62cd8ba1',
     indexName: 'telefunc',
   },
+
+  docsDir: 'packages/telefunc.com',
+
+  topNavigation: <TopNavigation />,
 } satisfies Config
