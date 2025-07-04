17f89e3acf351d6728480fbb6446d84266e02d9a
brillout <github.com@brillout.com>
2025-07-04 07:52:44 +0000

    Deploying to gh-pages from @ brillout/telefunc@299a159d87e9e35590110c0f8f7811b5ec2707fb 🚀

---
+++ assets/entries/pages_httpHeaders.CLJjgMEN.js
@@ -0,0 +1,21 @@
+import{o,a}from"../chunks/chunk-Dy3mUagX.js";import{j as e}from"../chunks/chunk-Cv4Or4mx.js";import{L as r}from"../chunks/chunk-BL3rSK6L.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import{M as i}from"../chunks/chunk-Dsij9-KT.js";/* empty css                      */const s=[{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function l(t){const n={a:"a",code:"code",figure:"figure",li:"li",p:"p",pre:"pre",span:"span",strong:"strong",ul:"ul",...t.components};return e.jsxs(e.Fragment,{children:[e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Environment"}),": client."]}),`
+`,e.jsx(n.p,{children:"Send additional HTTP headers to be sent along Telefunc HTTP requests."}),`
+`,e.jsx(n.p,{children:"Usually used for sending authentication headers."}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// Environment: client"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(n.span,{style:{color:"#24292E"},children:" { config } "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(n.span,{style:{color:"#032F62"},children:" 'telefunc/client'"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"config.httpHeaders "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"="}),e.jsx(n.span,{style:{color:"#24292E"},children:" {"})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"  Authorization: "}),e.jsx(n.span,{style:{color:"#032F62"},children:"`Bearer ${"}),e.jsx(n.span,{style:{color:"#24292E"},children:"token"}),e.jsx(n.span,{style:{color:"#032F62"},children:"}`"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#24292E"},children:"}"})})]})})}),`
+`,e.jsx(i,{}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(r,{href:"/fetch"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(r,{href:"/permissions"}),`
+`]}),`
+`,e.jsx(n.li,{children:e.jsxs(n.a,{href:"https://github.com/brillout/telefunc/issues/167",children:["#167 - Dynamic ",e.jsx(n.code,{children:"httpHeaders"}),"?"]})}),`
+`]})]})}function d(t={}){const{wrapper:n}=t.components||{};return n?e.jsx(n,{...t,children:e.jsx(l,{...t})}):l(t)}const c=Object.freeze(Object.defineProperty({__proto__:null,default:d,pageSectionsExport:s},Symbol.toStringTag,{value:"Module"})),C={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},onCreatePageContext:{type:"cumulative",definedAtData:[{filePathToShowToUser:"@brillout/docpress/renderer/onCreatePageContext",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:o}]},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/httpHeaders/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:c}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}}};export{C as configValuesSerialized};
