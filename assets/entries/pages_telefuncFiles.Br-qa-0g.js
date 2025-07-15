cbed12cc292d61f2d0f64b2d23cfc0d9862aa788
brillout <github.com@brillout.com>
2025-07-15 21:42:28 +0000

    Deploying to gh-pages from @ brillout/telefunc@2e0011479dccd4ad597d101f84c8a762b2d83f52 🚀

---
+++ assets/entries/pages_telefuncFiles.Br-qa-0g.js
@@ -0,0 +1,35 @@
+import{o as s,a as t}from"../chunks/chunk-DLgz-fBY.js";import{j as e}from"../chunks/chunk-BO5J93vE.js";import{L as o}from"../chunks/chunk-CMmf5thm.js";/* empty css                      */import{W as i}from"../chunks/chunk-BA0MW5mX.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import{M as a}from"../chunks/chunk-Bb7GaOxs.js";/* empty css                      *//* empty css                      */const d=[{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function l(r){const n={code:"code",figure:"figure",li:"li",p:"p",pre:"pre",span:"span",strong:"strong",ul:"ul",...r.components};return e.jsxs(e.Fragment,{children:[e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Environment"}),": server."]}),`
+`,e.jsx(i,{children:"This is a beta feature."}),`
+`,e.jsxs(n.p,{children:["If you don't use Telefunc's transformer on your server-side (see ",e.jsx(o,{href:"/transformer"}),") then you need to provide the list of your ",e.jsx(n.code,{children:".telefunc.js"})," files to Telefunc."]}),`
+`,e.jsx(n.p,{children:"You do so by using:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"config.telefuncFiles"})}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(o,{href:"/root",children:e.jsx(n.code,{children:"config.root"})}),`
+`]}),`
+`]}),`
+`,e.jsx(n.figure,{"data-rehype-pretty-code-figure":"",children:e.jsx(n.pre,{style:{backgroundColor:"#fff",color:"#24292e"},tabIndex:"0","data-language":"js","data-theme":"github-light",children:e.jsxs(n.code,{"data-language":"js","data-theme":"github-light",style:{display:"grid"},children:[e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// Environment: server"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#D73A49"},children:"import"}),e.jsx(n.span,{style:{color:"#24292E"},children:" { config } "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"from"}),e.jsx(n.span,{style:{color:"#032F62"},children:" 'telefunc'"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// List of telefunc files"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"config.telefuncFiles "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"="}),e.jsx(n.span,{style:{color:"#24292E"},children:" ["})]}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"  require."}),e.jsx(n.span,{style:{color:"#6F42C1"},children:"resolve"}),e.jsx(n.span,{style:{color:"#24292E"},children:"("}),e.jsx(n.span,{style:{color:"#032F62"},children:"'./hello.telefunc.mjs'"}),e.jsx(n.span,{style:{color:"#24292E"},children:")"})]}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#24292E"},children:"]"})}),`
+`,e.jsx(n.span,{"data-line":"",children:" "}),`
+`,e.jsx(n.span,{"data-line":"",children:e.jsx(n.span,{style:{color:"#6A737D"},children:"// The root directory of the project"})}),`
+`,e.jsxs(n.span,{"data-line":"",children:[e.jsx(n.span,{style:{color:"#24292E"},children:"config.root "}),e.jsx(n.span,{style:{color:"#D73A49"},children:"="}),e.jsx(n.span,{style:{color:"#24292E"},children:" __dirname"})]})]})})}),`
+`,e.jsxs(n.p,{children:["See also ",e.jsx(o,{href:"/root",children:e.jsx(n.code,{children:"config.root"})}),"."]}),`
+`,e.jsx(a,{}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(o,{href:"/root"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(o,{href:"/root#monorepo"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(o,{href:"/transformer"}),`
+`]}),`
+`]})]})}function c(r={}){const{wrapper:n}=r.components||{};return n?e.jsx(n,{...r,children:e.jsx(l,{...r})}):l(r)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:c,pageSectionsExport:d},Symbol.toStringTag,{value:"Module"})),D={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:t}},onCreatePageContext:{type:"cumulative",definedAtData:[{filePathToShowToUser:"@brillout/docpress/renderer/onCreatePageContext",fileExportPathToShowToUser:[]}],valueSerialized:[{type:"pointer-import",value:s}]},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/telefuncFiles/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}}};export{D as configValuesSerialized};
