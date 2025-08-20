4bab63c6fbb2ba7701fd2642af4ccd9b2dc58480
Romuald Brillout <git@brillout.com>
2025-08-20 21:55:48 +0200

    use requireResolve utils

--- packages/telefunc/node/vite/plugins/importBuild.ts
+++ packages/telefunc/node/vite/plugins/importBuild.ts
@@ -2,12 +2,10 @@ export { importBuild }
 
 import { serverProductionEntryPlugin } from '@brillout/vite-plugin-server-entry/plugin'
 import type { Plugin, ResolvedConfig } from 'vite'
-import { assert, assertPosixPath, projectInfo, toPosixPath } from '../utils.js'
+import { assert, assertPosixPath, projectInfo, requireResolveDistFile } from '../utils.js'
 import path from 'node:path'
 import { getTelefuncManifest } from './importBuild/getTelefuncManifest.js'
 import { getOutDirAbsolute } from '../getOutDirs.js'
-import { createRequire } from 'node:module'
-const require_ = createRequire(import.meta.url)
 
 function importBuild(): Plugin[] {
   let config: ResolvedConfig
@@ -51,9 +49,8 @@ function getServerProductionEntryCode(config: ResolvedConfig) {
 
 function getImportPath(config: ResolvedConfig) {
   // We resolve filePathAbsolute even if we don't use it: we use require.resolve() as an assertion that the relative path is correct
-  const filePathAbsolute = toPosixPath(
-    // [RELATIVE_PATH_FROM_DIST] Current file: node_modules/telefunc/dist/node/vite/plugins/importBuild.js
-    require_.resolve(`../../../../dist/node/server/runTelefunc/loadTelefuncFilesUsingVite/loadBuildEntry.js`),
+  const filePathAbsolute = requireResolveDistFile(
+    'dist/node/server/runTelefunc/loadTelefuncFilesUsingVite/loadBuildEntry.js',
   )
   if (
     // Let's implement a new config if a user needs the import to be a relative path instead of 'telefunc/__internal/loadImportBuild' (AFAIK there is no use case for relative paths for Telefunc)
