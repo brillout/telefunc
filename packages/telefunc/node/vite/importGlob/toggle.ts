f814436166376fb82d54abde774498cfce9bded7
Romuald Brillout <git@brillout.com>
2025-08-20 19:39:06 +0200

    align with Vike

--- packages/telefunc/node/vite/importGlob/toggle.ts
+++ packages/telefunc/node/vite/importGlob/toggle.ts
@@ -3,7 +3,7 @@ export { importGlobOn }
 
 import { writeFileSync } from 'node:fs'
 // We import node/server/utils.js instead of node/vite/utils.js because importGlobOff() is imported by webpack/loader.ts
-import { scriptFileExtensions, toPosixPath } from '../../server/utils.js'
+import { javaScriptFileExtensionPattern, toPosixPath } from '../../server/utils.js'
 import { createRequire } from 'node:module'
 const require_ = createRequire(import.meta.url)
 const telefuncFilesGlobFilePath = toPosixPath(require_.resolve('./telefuncFilesGlob.js'))
@@ -12,7 +12,7 @@ globalThis._telefunc.telefuncFilesGlobFilePath = telefuncFilesGlobFilePath
 declare global {
   var _telefunc: undefined | { telefuncFilesGlobFilePath?: string }
 }
-const importGlob = `import.meta.glob("/**/*.telefunc.${scriptFileExtensions}")`
+const importGlob = `import.meta.glob("/**/*.telefunc.${javaScriptFileExtensionPattern}")`
 
 function importGlobOff() {
   writeFileSync(telefuncFilesGlobFilePath, ['exports.importGlobOff = true', ''].join('\n'))
