ea36d0ab25ba8ba7db8bf69517102a37557c7bde
Romuald Brillout <git@brillout.com>
2025-08-21 15:21:11 +0200

    minor refactor

--- packages/telefunc/node/vite/plugins/virtualModule.ts
+++ packages/telefunc/node/vite/plugins/virtualModule.ts
@@ -4,12 +4,10 @@ import type { Plugin } from 'vite'
 import { javaScriptFileExtensionPattern } from '../utils.js'
 
 const VIRTUAL_MODULE_ID = 'virtual:telefunc-files-glob'
+const moduleContent = `export const telefuncFilesGlob = import.meta.glob("/**/*.telefunc.${javaScriptFileExtensionPattern}");`
 
 function virtualModule(): Plugin {
   const resolvedId = '\0' + VIRTUAL_MODULE_ID
-  const importGlobPattern = `import.meta.glob("/**/*.telefunc.${javaScriptFileExtensionPattern}")`
-  const moduleContent = `export const telefuncFilesGlob = ${importGlobPattern};`
-
   return {
     name: 'telefunc:virtualModule',
     resolveId: (id) => (id === VIRTUAL_MODULE_ID ? resolvedId : undefined),
