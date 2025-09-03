28c649808e6046440bb5a24bbffdda6194440f51
Rom <git@brillout.com>
2025-09-04 00:36:31 +0200

    perf: use Vite hook filters (#208)

--- packages/telefunc/node/vite/plugins/pluginVirtualFileEntry.ts
+++ packages/telefunc/node/vite/plugins/pluginVirtualFileEntry.ts
@@ -1,21 +1,30 @@
 export { pluginVirtualFileEntry }
 
 import type { Plugin } from 'vite'
-import { javaScriptFileExtensionPattern } from '../utils.js'
+import { assert, escapeRegex, javaScriptFileExtensionPattern } from '../utils.js'
 import { VIRTUAL_FILE_ENTRY_ID } from './pluginVirtualFileEntry/VIRTUAL_FILE_ENTRY_ID.js'
-
 const moduleContent = `export const telefuncFilesGlob = import.meta.glob("/**/*.telefunc.${javaScriptFileExtensionPattern}");`
+const resolvedId = '\0' + VIRTUAL_FILE_ENTRY_ID
 
 function pluginVirtualFileEntry(): Plugin {
-  const resolvedId = '\0' + VIRTUAL_FILE_ENTRY_ID
   return {
     name: 'telefunc:pluginVirtualFileEntry',
     resolveId: {
+      filter: {
+        id: new RegExp(`^${escapeRegex(VIRTUAL_FILE_ENTRY_ID)}$`),
+      },
       handler(id) {
-        return id === VIRTUAL_FILE_ENTRY_ID ? resolvedId : undefined
+        assert(id === VIRTUAL_FILE_ENTRY_ID)
+        return resolvedId
       },
     },
     load: {
+      filter: {
+        /* I don't know why this doesn't work:
+        id: resolvedId,
+        */
+        id: new RegExp(`^${escapeRegex(resolvedId)}$`),
+      },
       handler(id) {
         return id === resolvedId ? moduleContent : undefined
       },
