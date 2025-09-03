4df9512ee322d9e395bf1fd4bfaf23e0bdf985eb
Romuald Brillout <git@brillout.com>
2025-09-03 23:41:16 +0200

    Revert "require vite 6.3.0"
    This reverts commit e7d251e9c649629edace807a3c4bf0c668335dcc.

--- packages/telefunc/node/vite/onLoad.ts
+++ packages/telefunc/node/vite/onLoad.ts
@@ -7,9 +7,9 @@ import { version } from 'vite'
 function onLoad() {
   assertIsNotBrowser()
   // package.json#peerDependencies isn't enough as users often ignore it
-  // This assertion isn't reliable: the user may still use a Vite version older than 6.3.0 — see https://github.com/vitejs/vite/pull/19355
+  // This assertion isn't reliable: the user may still use a Vite version older than 6.0.0 — see https://github.com/vitejs/vite/pull/19355
   // TO-DO/eventually: let's also use this.meta.viteVersion
   // - https://github.com/vitejs/vite/pull/20088
   // - https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md#700-2025-06-24
-  assertVersion('Vite', version, '6.3.0')
+  assertVersion('Vite', version, '6.0.0')
 }
