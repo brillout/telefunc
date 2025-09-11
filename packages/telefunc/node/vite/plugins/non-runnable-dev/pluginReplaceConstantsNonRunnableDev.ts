b607d974cc150693d4e3459c1e2f967ce4873554
Romuald Brillout <git@brillout.com>
2025-09-11 23:50:00 +0200

    minor refactor

--- packages/telefunc/node/vite/plugins/non-runnable-dev/pluginReplaceConstantsNonRunnableDev.ts
+++ packages/telefunc/node/vite/plugins/non-runnable-dev/pluginReplaceConstantsNonRunnableDev.ts
@@ -7,17 +7,17 @@ import type { Plugin } from 'vite'
 // - We cannot use [`define`](https://vite.dev/config/shared-options.html#define) because we don't have access to `this.environment` and therefore we cannot call `isRunnableDevEnvironment(this.environment)` inside a configEnvironment() hook.
 // - We cannot use [`filter.id`](https://rolldown.rs/plugins/hook-filters) because Vite's optimizeDeps bundles packages (e.g. `vike` or `telefunc`) into node_modules/.vite/deps_ssr/chunk-WBC5FHD7.js
 
-const DYNAMIC_IMPORT = '__TELEFUNC__DYNAMIC_IMPORT'
 const IS_NON_RUNNABLE_DEV = 'globalThis.__TELEFUNC__IS_NON_RUNNABLE_DEV'
+const DYNAMIC_IMPORT = '__TELEFUNC__DYNAMIC_IMPORT'
 
 declare global {
-  var __TELEFUNC__DYNAMIC_IMPORT: (module: `virtual:${string}`) => Promise<Record<string, unknown>>
   var __TELEFUNC__IS_NON_RUNNABLE_DEV: undefined | true
+  var __TELEFUNC__DYNAMIC_IMPORT: (module: `virtual:${string}`) => Promise<Record<string, unknown>>
 }
 function pluginReplaceConstantsNonRunnableDev(): Plugin[] {
   return [
     {
-      name: 'telefunc:pluginReplaceConstantsNonRunnableDev:1',
+      name: 'telefunc:pluginReplaceConstantsNonRunnableDev:IS_NON_RUNNABLE_DEV',
       apply: (_, configEnv) => isDevCheck(configEnv),
       transform: {
         filter: {
@@ -34,7 +34,7 @@ function pluginReplaceConstantsNonRunnableDev(): Plugin[] {
       },
     },
     {
-      name: 'telefunc:pluginReplaceConstantsNonRunnableDev:2',
+      name: 'telefunc:pluginReplaceConstantsNonRunnableDev:DYNAMIC_IMPORT',
       apply: (_, configEnv) => isDevCheck(configEnv),
       transform: {
         filter: {
