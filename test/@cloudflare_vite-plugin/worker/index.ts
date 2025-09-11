1246b922597d09b71f48aeff0785c0ea56ebc2b9
Romuald Brillout <git@brillout.com>
2025-09-11 16:14:41 +0200

    fmt

--- test/@cloudflare_vite-plugin/worker/index.ts
+++ test/@cloudflare_vite-plugin/worker/index.ts
@@ -67,7 +67,7 @@ export class Counter extends DurableObject {
   }
 
   async increment(amount = 1) {
-    let value = (await this.ctx.storage.get('value')) as (undefined | number) || 0
+    let value = ((await this.ctx.storage.get('value')) as undefined | number) || 0
     value += amount
     // You do not have to worry about a concurrent request having modified the value in storage.
     // "input gates" will automatically protect against unwanted concurrency.
@@ -77,7 +77,7 @@ export class Counter extends DurableObject {
   }
 
   async decrement(amount = 1) {
-    let value = (await this.ctx.storage.get('value')) as (undefined | number) || 0
+    let value = ((await this.ctx.storage.get('value')) as undefined | number) || 0
     value -= amount
     await this.ctx.storage.put('value', value)
     return value
