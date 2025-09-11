a78f612d95e2e1eba8976153ad316144af0ed0cc
Romuald Brillout <git@brillout.com>
2025-09-11 23:21:06 +0200

    minor refactor

--- packages/telefunc/node/vite/plugins/non-runnable-dev/pluginReplaceConstantsNonRunnableDev.ts
+++ packages/telefunc/node/vite/plugins/non-runnable-dev/pluginReplaceConstantsNonRunnableDev.ts
@@ -7,8 +7,8 @@ import { getMagicString } from '../../../shared/getMagicString.js'
 // - We cannot use [`define`](https://vite.dev/config/shared-options.html#define) because we don't have access to `this.environment` and therefore we cannot call `isRunnableDevEnvironment(this.environment)` inside a configEnvironment() hook.
 // - We cannot use [`filter.id`](https://rolldown.rs/plugins/hook-filters) because Vite's optimizeDeps bundles packages (e.g. `vike` or `telefunc`) into node_modules/.vite/deps_ssr/chunk-WBC5FHD7.js
 
-const __TELEFUNC__DYNAMIC_IMPORT = '__TELEFUNC__DYNAMIC_IMPORT'
-const __TELEFUNC__IS_NON_RUNNABLE_DEV = 'globalThis.__TELEFUNC__IS_NON_RUNNABLE_DEV'
+const DYNAMIC_IMPORT = '__TELEFUNC__DYNAMIC_IMPORT'
+const IS_NON_RUNNABLE_DEV = 'globalThis.__TELEFUNC__IS_NON_RUNNABLE_DEV'
 
 declare global {
   var __TELEFUNC__DYNAMIC_IMPORT: (module: `virtual:${string}`) => Promise<Record<string, unknown>>
@@ -22,13 +22,13 @@ function pluginReplaceConstantsNonRunnableDev(): Plugin[] {
       transform: {
         filter: {
           code: {
-            include: __TELEFUNC__IS_NON_RUNNABLE_DEV,
+            include: IS_NON_RUNNABLE_DEV,
           },
         },
         handler(code, id) {
           if (isRunnableDevEnvironment(this.environment)) return
           const { magicString, getMagicStringResult } = getMagicString(code, id)
-          magicString.replaceAll(__TELEFUNC__IS_NON_RUNNABLE_DEV, JSON.stringify(true))
+          magicString.replaceAll(IS_NON_RUNNABLE_DEV, JSON.stringify(true))
           return getMagicStringResult()
         },
       },
@@ -39,13 +39,13 @@ function pluginReplaceConstantsNonRunnableDev(): Plugin[] {
       transform: {
         filter: {
           code: {
-            include: __TELEFUNC__DYNAMIC_IMPORT,
+            include: DYNAMIC_IMPORT,
           },
         },
         handler(code, id) {
           if (isRunnableDevEnvironment(this.environment)) return
           const { magicString, getMagicStringResult } = getMagicString(code, id)
-          magicString.replaceAll(__TELEFUNC__DYNAMIC_IMPORT, 'import')
+          magicString.replaceAll(DYNAMIC_IMPORT, 'import')
           return getMagicStringResult()
         },
       },
