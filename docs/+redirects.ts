a8184b629f447b04a4a57d93b5e47f8eaa47fba6
Romuald Brillout <git@brillout.com>
2025-09-18 13:06:03 +0200

    fmt

--- docs/+redirects.ts
+++ docs/+redirects.ts
@@ -10,5 +10,5 @@ type RedirectsURL = RemoveHash<(typeof redirects)[keyof typeof redirects]>
 checkType<HeadingsURL>(0 as any as RedirectsURL)
 
 const redirects = {
-  '/remix': '/react-router'
+  '/remix': '/react-router',
 } as const satisfies Config['redirects']
