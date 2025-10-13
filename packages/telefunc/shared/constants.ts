633fae44131910f457b500668b75128e46cbb0af
Romuald Brillout <git@brillout.com>
2025-10-13 13:50:56 +0200

    minor refactor

--- packages/telefunc/shared/constants.ts
+++ packages/telefunc/shared/constants.ts
@@ -1,9 +1,5 @@
 export const STATUS_CODE_SUCCESS = 200
 export const STATUS_CODE_MALFORMED_REQUEST = 400
-/**
- * HTTP Response for:
- * - `throw Abort()`
- */
 export const STATUS_CODE_THROW_ABORT = 403 // "Forbidden"
 export const STATUS_CODE_SHIELD_VALIDATION_ERROR = 422 // "Unprocessable Content"
 export const STATUS_CODE_INTERNAL_SERVER_ERROR = 500
