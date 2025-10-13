ad1ade8f2c694cba40dabcfaba39804a105a1ee5
Romuald Brillout <git@brillout.com>
2025-10-13 13:51:55 +0200

    comment

--- packages/telefunc/shared/constants.ts
+++ packages/telefunc/shared/constants.ts
@@ -1,5 +1,5 @@
-export const STATUS_CODE_SUCCESS = 200
-export const STATUS_CODE_MALFORMED_REQUEST = 400
+export const STATUS_CODE_SUCCESS = 200 // "OK"
+export const STATUS_CODE_MALFORMED_REQUEST = 400 // "Bad Request"
 export const STATUS_CODE_THROW_ABORT = 403 // "Forbidden"
 export const STATUS_CODE_SHIELD_VALIDATION_ERROR = 422 // "Unprocessable Content"
-export const STATUS_CODE_INTERNAL_SERVER_ERROR = 500
+export const STATUS_CODE_INTERNAL_SERVER_ERROR = 500  // "Internal Server Error"
