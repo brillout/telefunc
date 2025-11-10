d9967bd1541d6f6eef7ffdf793ce9bff5e4a16c3
Romuald Brillout <git@brillout.com>
2025-11-10 23:51:53 +0100

    fix: workaround Vite bug upon import picocolors in utils/assert.ts

--- packages/telefunc/utils/assert.ts
+++ packages/telefunc/utils/assert.ts
@@ -8,7 +8,9 @@ export { errorPrefix }
 import { createErrorWithCleanStackTrace } from './createErrorWithCleanStackTrace.js'
 import { getGlobalObject } from './getGlobalObject.js'
 import { projectInfo } from './projectInfo.js'
+/* We cannot import it here because of what seems to be a Vite bug https://github.com/vitejs/vite/issues/21088
 import pc from '@brillout/picocolors'
+*/
 
 const errorPrefix = `[telefunc@${projectInfo.projectVersion}]`
 const internalErrorPrefix = red(`${errorPrefix}[Bug]`)
@@ -108,6 +110,17 @@ function assertInfo(condition: unknown, errorMessage: string, { onlyOnce }: { on
   console.log(msg)
 }
 
+//*
+function red<Str extends string>(str: Str) {
+  return str
+}
+function yellow<Str extends string>(str: Str) {
+  return str
+}
+function blue<Str extends string>(str: Str) {
+  return str
+}
+/*/
 function red<Str extends string>(str: Str) {
   return pc.red(pc.bold(str))
 }
@@ -117,3 +130,4 @@ function yellow<Str extends string>(str: Str) {
 function blue<Str extends string>(str: Str) {
   return pc.blue(pc.bold(str))
 }
+/*/
