1e11b63f811ed715ee06b461a917bcd051d4077d
Romuald Brillout <git@brillout.com>
2026-02-18 09:54:27 +0100

    update to-do

--- packages/telefunc/client/index.ts
+++ packages/telefunc/client/index.ts
@@ -1,5 +1,6 @@
 import { config } from './clientConfig.js'
 export { config }
+// TO-DO/next-major: remove this redundant export
 export { config as telefuncConfig }
 export { onAbort, onTelefunctionRemoteCallError } from './remoteTelefunctionCall/onAbort.js'
 export type { TelefunctionError } from './TelefunctionError.js'
