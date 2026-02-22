e6d4ebfe52ee32a02181fe6724c89cc223418070
Romuald Brillout <git@brillout.com>
2026-02-22 16:03:29 +0100

    update to-do

--- packages/telefunc/node/server/streaming/index.ts
+++ packages/telefunc/node/server/streaming/index.ts
@@ -1,3 +1,4 @@
+// TODO/ai remove this file
 export { LazyBlob, LazyFile, isLazyBlob, isLazyFile } from './lazyFile.js'
 export { MultipartParser } from './multipartParser.js'
 export { MultipartReader } from './multipartReader.js'
