3971ac53cd5a373afb9b419fe400bd0048b458e3
Romuald Brillout <git@brillout.com>
2026-02-22 14:22:15 +0100

    rename main field

--- packages/telefunc/shared/multipart/constants.ts
+++ packages/telefunc/shared/multipart/constants.ts
@@ -1,4 +1,4 @@
-export const FORM_DATA_MAIN_FIELD = '__telefunc'
+export const FORM_DATA_MAIN_FIELD = '__telefunc_main'
 export const MULTIPART_PLACEHOLDER_KEY = '__telefunc_multipart'
 export const SERIALIZER_PREFIX_FILE = '!TelefuncFile:'
 export const SERIALIZER_PREFIX_BLOB = '!TelefuncBlob:'
