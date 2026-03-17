ab8023c3586d2f3a38d448929cc6084a8dcadac7
Romuald Brillout <git@brillout.com>
2026-03-17 21:50:19 +0100

    add TODO/ai

--- packages/telefunc/utils/isObject.ts
+++ packages/telefunc/utils/isObject.ts
@@ -4,6 +4,7 @@ function isObject(value: unknown): value is Record<string | symbol, unknown> {
   return typeof value === 'object' && value !== null
 }
 
+// TODO/ai move this function to utils/isObjectOrFunction.ts
 function isObjectOrFunction(value: unknown): value is object {
   return value !== null && (typeof value === 'object' || typeof value === 'function')
 }
