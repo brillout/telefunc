2b3b54907b17a1ae0b069573790a59afea11b4b2
Romuald Brillout <git@brillout.com>
2026-03-17 22:00:28 +0100

    isConnectionError

--- packages/telefunc/client/ConnectionError.ts
+++ packages/telefunc/client/ConnectionError.ts
@@ -1,6 +1,7 @@
 export { ConnectionError }
 
 class ConnectionError extends Error {
+  isConnectionError = true as const
   constructor(message = 'No Server Connection') {
     super(message)
     Object.setPrototypeOf(this, new.target.prototype)
