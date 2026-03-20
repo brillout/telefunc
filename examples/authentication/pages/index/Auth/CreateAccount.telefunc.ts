7d3b59fd1441838695ed1c63cd8b037a2ba55058
ambergristle <101149854+ambergristle@users.noreply.github.com>
2026-03-20 10:56:35 -0700

    chore: format updated examples

--- examples/authentication/pages/index/Auth/CreateAccount.telefunc.ts
+++ examples/authentication/pages/index/Auth/CreateAccount.telefunc.ts
@@ -3,9 +3,12 @@ export { onCreateAccount }
 import { shield } from 'telefunc'
 import { UserModel } from '#app/db'
 
-const onCreateAccount = shield(async function (name) {
-  UserModel.add(name)
-  const users = UserModel.getAll()
-  const userList = Object.values(users)
-  return userList
-}, [shield.type.string])
+const onCreateAccount = shield(
+  async function (name) {
+    UserModel.add(name)
+    const users = UserModel.getAll()
+    const userList = Object.values(users)
+    return userList
+  },
+  [shield.type.string],
+)
--- examples/babel/hello.telefunc.mjs
+++ examples/babel/hello.telefunc.mjs
@@ -3,7 +3,10 @@ export { hello }
 import { shield } from 'telefunc'
 const t = shield.type
 
-const hello = shield(async function ({ name }) {
-  const message = `Welcome ${name}`
-  return { message }
-}, [{ name: t.string }])
+const hello = shield(
+  async function ({ name }) {
+    const message = `Welcome ${name}`
+    return { message }
+  },
+  [{ name: t.string }],
+)
